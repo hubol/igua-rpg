46ab9aa9aa541ea1f12ba107594a606e900a85ba
Hubol <hubol.gordon@gmail.com>
2020-04-08 19:59:46 -0500

    Initial commit

---
+++ src/sounds.ts
@@ -0,0 +1,7 @@
+import { Howl } from 'howler';
+    
+// This file is generated. Do not touch.
+
+export const Hallelujah = new Howl({
+    src: [require("./sound/hallelujah.ogg"), require("./sound/hallelujah.mp3")]
+});
