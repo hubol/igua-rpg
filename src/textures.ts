b1e9d407d2316a00ce2d2414ca15fe0907608109
Hubol <hubol.gordon@gmail.com>
2020-04-09 13:35:25 -0500

    Add valuable textures

--- src/textures.ts
+++ src/textures.ts
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
     
 // This file is generated. Do not touch.
 
+export let BlueValuable: PIXI.Texture = undefined as unknown as PIXI.Texture;
 export let CharacterBody: PIXI.Texture = undefined as unknown as PIXI.Texture;
 export let CharacterCrest: PIXI.Texture = undefined as unknown as PIXI.Texture;
 export let CharacterFootBackLeft: PIXI.Texture = undefined as unknown as PIXI.Texture;
@@ -9,11 +10,15 @@ export let CharacterFootBackRight: PIXI.Texture = undefined as unknown as PIXI.T
 export let CharacterFootFrontLeft: PIXI.Texture = undefined as unknown as PIXI.Texture;
 export let CharacterFootFrontRight: PIXI.Texture = undefined as unknown as PIXI.Texture;
 export let CharacterHead: PIXI.Texture = undefined as unknown as PIXI.Texture;
+export let OrangeValuable: PIXI.Texture = undefined as unknown as PIXI.Texture;
 
 
 export function loadTexturesAsync()
 {
     const loader = new PIXI.Loader();
+
+    const BlueValuablePath = require("./images/blue valuable.png");
+    loader.add(BlueValuablePath); 
 
     const CharacterBodyPath = require("./images/character/body.png");
     loader.add(CharacterBodyPath); 
@@ -36,10 +41,14 @@ export function loadTexturesAsync()
     const CharacterHeadPath = require("./images/character/head.png");
     loader.add(CharacterHeadPath); 
 
+    const OrangeValuablePath = require("./images/orange valuable.png");
+    loader.add(OrangeValuablePath); 
+
     
     return new Promise(resolve =>
     {
         loader.load((_, resources) => {
+            BlueValuable = resources[BlueValuablePath]?.texture as PIXI.Texture;
             CharacterBody = resources[CharacterBodyPath]?.texture as PIXI.Texture;
             CharacterCrest = resources[CharacterCrestPath]?.texture as PIXI.Texture;
             CharacterFootBackLeft = resources[CharacterFootBackLeftPath]?.texture as PIXI.Texture;
@@ -47,6 +56,7 @@ export function loadTexturesAsync()
             CharacterFootFrontLeft = resources[CharacterFootFrontLeftPath]?.texture as PIXI.Texture;
             CharacterFootFrontRight = resources[CharacterFootFrontRightPath]?.texture as PIXI.Texture;
             CharacterHead = resources[CharacterHeadPath]?.texture as PIXI.Texture;
+            OrangeValuable = resources[OrangeValuablePath]?.texture as PIXI.Texture;
 
             resolve();
         });
