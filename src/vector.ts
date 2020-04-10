d540b9bc28593352e7e3d6302f74c87045fda53e
Hubol <hubol.gordon@gmail.com>
2020-04-10 10:36:43 -0500

    Some js -> ts
    Former-commit-id: 6ef191725b0c650f0508da1daf155a7b86e8b361

--- src/vector.ts
+++ src/vector.ts
@@ -1,19 +1,25 @@
-export function perpendicular(vec) {
+export interface Vector
+{
+    x: number;
+    y: number;
+}
+
+export function perpendicular(vec: Vector) {
     const tmp = vec.x;
     vec.x = vec.y;
     vec.y = -tmp;
     return vec;
 }
 
-export function dot(vec1, vec2) {
+export function dot(vec1: Vector, vec2: Vector) {
     return vec1.x * vec2.x + vec1.y * vec2.y;
 }
 
-export function distance(vec1, vec2) {
+export function distance(vec1: Vector, vec2: Vector) {
     return Math.sqrt(Math.pow(vec1.x - vec2.x, 2) + Math.pow(vec1.y - vec2.y, 2));
 }
 
-export function normalize(vec) {
+export function normalize(vec: Vector) {
     const d = (Math.sqrt(Math.pow(vec.x, 2) + Math.pow(vec.y, 2)));
     if (d === 0) {
         vec.x = 1;
