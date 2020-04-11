aebbbe8f6a76d2d887f8697501daa6f7173dede8
Hubol <hubol.gordon@gmail.com>
2020-04-11 02:09:51 -0500

    Pull out areRectanglesOverlapping

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -11,3 +11,19 @@ export function approachLinear(a: number, b: number, factor: number)
         return Math.max(a - factor, b);
     return b;
 }
+
+export function areRectanglesOverlapping(a: Rectangle, b: Rectangle)
+{
+    return a.x + a.width > b.x
+        && a.x < b.x + b.width
+        && a.y + a.height > b.y
+        && a.y < b.y + b.height;
+}
+
+interface Rectangle
+{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
