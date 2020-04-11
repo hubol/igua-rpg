3e4b6a07f7f5c90d812887ee118821e534d0d3ce
Hubol <hubol.gordon@gmail.com>
2020-04-11 02:36:47 -0500

    Implement naive camera

---
+++ src/playerCamera.ts
@@ -0,0 +1,30 @@
+import {session} from "./session";
+import {app} from "./app";
+import {lerpVector} from "./utils/math";
+
+let target = { x: 0, y: 0 };
+
+export function stepPlayerCamera()
+{
+    if (!session.control.cameraFollowPlayer)
+        return;
+
+    const margin = 80;
+
+    const x0 = app.camera.x + margin;
+    const y0 = app.camera.y + margin;
+    const x1 = app.camera.x + app.width - margin;
+    const y1 = app.camera.y + app.height - margin;
+
+    if (app.player.x - x0 < 0)
+        target.x = app.camera.x + app.player.x - x0;
+    else if (app.player.x - x1 > 0)
+        target.x = app.camera.x + app.player.x - x1;
+
+    if (app.player.y - y0 < 0)
+        target.y = app.camera.y + app.player.y - y0;
+    else if (app.player.y - y1 > 0)
+        target.y = app.camera.y + app.player.y - y1;
+
+    lerpVector(app.camera, target, 0.33);
+}
