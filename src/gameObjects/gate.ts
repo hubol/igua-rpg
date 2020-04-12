fd3dd7c3924c1559519bc59af35dc9bcf84a8931
Hubol <hubol.gordon@gmail.com>
2020-04-12 18:59:35 -0500

    Gate now loads the correct level
    Former-commit-id: 1e92ad04cb5fbf4f1603c36e656b734926179904

--- src/gameObjects/gate.ts
+++ src/gameObjects/gate.ts
@@ -2,6 +2,8 @@ import {Container} from "pixi.js";
 import {game} from "../game";
 import {loadLevel, unloadLevel} from "../level";
 import {areRectanglesOverlapping} from "../utils/math";
+import * as levels from "../levels";
+import {Test} from "../levels";
 
 export function gate(x, y, width, height, destination: { checkpointName, levelName })
 {
@@ -12,11 +14,18 @@ export function gate(x, y, width, height, destination: { checkpointName, levelNa
         if (areRectanglesOverlapping(game.player.rectangle, bounds))
         {
             unloadLevel();
-            loadLevel(require("../levels/right-test.json"));
+            loadLevel(getLevel(destination.levelName));
         }
     };
 
     container.on("added", () => game.ticker.add(step));
     container.on("removed", () => game.ticker.remove(step));
     return container;
+}
+
+function getLevel(name)
+{
+    if (name in levels)
+        return levels[name];
+    return Test;
 }
---
+++ src/levels.ts
@@ -0,0 +1,6 @@
+import * as RightTest_json from "./levels/RightTest.json"
+export const RightTest = RightTest_json;
+
+import * as Test_json from "./levels/test.json"
+export const Test = Test_json;
+
