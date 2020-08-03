b27f3353f44837aff2b7e152567a33a5a0c9292c
Hubol <hubol.gordon@gmail.com>
2020-08-03 12:25:45 -0500

    Naive bitmap font

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,8 +1,19 @@
 import {FontFamilyName, loadGoogleFontAsync} from "./utils/loadGoogleFont";
+import {BitmapFont, Texture} from "pixi.js";
+import {default as axios} from "axios";
 
 export let RobotFont: FontFamilyName;
+export let AcrobatixFont: BitmapFont;
 
 export async function loadFontsAsync()
 {
-    RobotFont = await loadGoogleFontAsync("Roboto");
+    [RobotFont, AcrobatixFont] = await Promise.all(
+        [loadGoogleFontAsync("Roboto"),
+            loadBitmapFontAsync(require("./fonts/Acrobatix.fnt"), Texture.from(require("./fonts/Acrobatix_0.png")))]);
+}
+
+async function loadBitmapFontAsync(fntUrl: string, texture: Texture)
+{
+    const fntData = (await axios.get(fntUrl)).data;
+    return BitmapFont.install(fntData, texture);
 }
