6fe32ac8666568356556da19a7c653d5f9ad879a
Hubol <hubol.gordon@gmail.com>
2020-08-03 12:29:04 -0500

    Remove loadGoogleFont and associated dependencies

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,15 +1,12 @@
-import {FontFamilyName, loadGoogleFontAsync} from "./utils/loadGoogleFont";
 import {BitmapFont, Texture} from "pixi.js";
 import {default as axios} from "axios";
 
-export let RobotFont: FontFamilyName;
 export let AcrobatixFont: BitmapFont;
 
 export async function loadFontsAsync()
 {
-    [RobotFont, AcrobatixFont] = await Promise.all(
-        [loadGoogleFontAsync("Roboto"),
-            loadBitmapFontAsync(require("./fonts/Acrobatix.fnt"), Texture.from(require("./fonts/Acrobatix_0.png")))]);
+    AcrobatixFont = await
+            loadBitmapFontAsync(require("./fonts/Acrobatix.fnt"), Texture.from(require("./fonts/Acrobatix_0.png")));
 }
 
 async function loadBitmapFontAsync(fntUrl: string, texture: Texture)
