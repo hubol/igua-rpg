0d8032bcdcea74f4cba318686eaf655c608be81d
Hubol <hubol.gordon@gmail.com>
2020-08-03 19:58:48 -0500

    Inventing small utilities to abbreviate creating npc styles

--- src/gameObjects/npc.ts
+++ src/gameObjects/npc.ts
@@ -4,14 +4,14 @@ import {
     CharacterMouthV, CharacterPupils, CharacterWhites, NpcCurvedCrest, NpcRedFoot, NpcRedFootRear,
     NpcWeirdBody
 } from "../textures";
-import {iguanaPuppet} from "../igua/iguanaPuppet";
+import {IguanaPuppet, iguanaPuppet} from "../igua/iguanaPuppet";
 import {game} from "../igua/game";
 import {distance} from "../utils/vector";
-import {iguanaEyes} from "../igua/iguanaEyes";
+import {IguanaEyes, iguanaEyes} from "../igua/iguanaEyes";
 
 export function npc(x, y)
 {
-    const puppet = npcPuppet();
+    const puppet = npcStyles[0]();
     puppet.x = x;
     puppet.y = y;
 
@@ -22,49 +22,67 @@ export function npc(x, y)
     });
 }
 
-function npcPuppet()
-{
-    const body = Sprite.from(NpcWeirdBody);
-    body.pivot.y += 4;
-    body.tint = 0xFF8A7C;
+let npcStyles: Array<() => IguanaPuppet> = [];
 
-    const frontLeftFoot = Sprite.from(NpcRedFoot);
-    frontLeftFoot.tint = 0xC7D7D7;
-    const frontRightFoot = Sprite.from(NpcRedFoot);
+npcStyles[0] = npcStyle(args => {
+    args.body = Sprite.from(NpcWeirdBody);
+    args.body.pivot.y += 4;
+    args.body.tint = 0xFF8A7C;
 
-    const backLeftFoot = Sprite.from(NpcRedFootRear);
-    backLeftFoot.tint = 0xC7D7D7;
-    backLeftFoot.pivot.y = 1;
-    const backRightFoot = Sprite.from(NpcRedFootRear);
-    backRightFoot.pivot.y = 1;
+    args.frontRightFoot = Sprite.from(NpcRedFoot);
 
-    const crest = Sprite.from(NpcCurvedCrest);
-    crest.pivot.set(-2, 2);
-    crest.tint = 0x75CAFF;
+    args.backRightFoot = Sprite.from(NpcRedFootRear);
+    args.backRightFoot.pivot.y = 1;
 
-    const head = new Container();
+    args.crest = Sprite.from(NpcCurvedCrest);
+    args.crest.pivot.set(-2, 2);
+    args.crest.tint = 0x75CAFF;
+
     const headSprite = Sprite.from(CharacterHead);
     headSprite.tint = 0x73C88C;
     const mouthSprite = Sprite.from(CharacterMouthV);
     mouthSprite.pivot.set(-10, -11);
     mouthSprite.tint = 0xA83F2F;
-    head.addChild(headSprite, mouthSprite);
-    head.cacheAsBitmap = true;
+    args.head.addChild(headSprite, mouthSprite);
 
     const pupils = Sprite.from(CharacterPupils);
     pupils.tint = 0xA83F2F;
     pupils.pivot.set(-2, -3);
 
-    const eyes = iguanaEyes({ eyelidColor: 0x5AA86E, pupils, eyeShape: Sprite.from(CharacterWhites) });
+    args.eyes = iguanaEyes({ eyelidColor: 0x5AA86E, pupils, eyeShape: Sprite.from(CharacterWhites) });
+});
 
-    return iguanaPuppet({
-        backRightFoot,
-        frontLeftFoot,
-        backLeftFoot,
-        body,
-        crest,
-        head,
-        eyes,
-        frontRightFoot
-    });
+function npcStyle(configure: (args: ConfigureNpcStyleArgs) => void)
+{
+    return () => {
+        const args = { head: new Container() } as ConfigureNpcStyleArgs;
+        configure(args);
+
+        if (!args.frontLeftFoot)
+        {
+            args.frontLeftFoot = Sprite.from(args.frontRightFoot.texture);
+            args.frontLeftFoot.tint = 0xC7D7D7;
+        }
+
+        if (!args.backLeftFoot)
+        {
+            args.backLeftFoot = Sprite.from(args.backRightFoot.texture);
+            args.backLeftFoot.tint = 0xC7D7D7;
+            args.backLeftFoot.pivot.copyFrom(args.backRightFoot.pivot);
+        }
+
+        return iguanaPuppet(args as any);
+    }
+}
+
+interface ConfigureNpcStyleArgs
+{
+    body: Sprite;
+    backLeftFoot?: Sprite;
+    backRightFoot: Sprite;
+    frontLeftFoot?: Sprite;
+    frontRightFoot: Sprite;
+    head: Container;
+    eyes: IguanaEyes;
+    crest: Sprite;
 }
