6a67f07e3202e129aa361214e9f2028db06c508f
Hubol <hubol.gordon@gmail.com>
2020-08-05 11:08:29 -0500

    "Test" more playCutscene behavior
    Former-commit-id: e0b8ddc456b350c66f8449a1172d8c8d375cde5b

--- src/cutscene/testScene.ts
+++ src/cutscene/testScene.ts
@@ -1,8 +1,16 @@
 import {show} from "./dialog";
+import {game} from "../igua/game";
 
 export async function testCutscene()
 {
     await show("Hi!!!");
     await show("Welcome to IguaRPG");
     await show("Very fun !!!");
+    game.cutscenePlayer.playCutscene(testCutscene2());
+}
+
+export async function testCutscene2()
+{
+    await show("HEY!!!!!! We love it");
+    throw new Error("help");
 }
