4d2db5448486a0e317df4a06324dde7975d86d9c
Hubol <hubol.gordon@gmail.com>
2020-08-05 23:20:36 -0500

    Reduce code duplication in waitForKey
    Former-commit-id: 3786445611ca575ca469df00f70f3bfde9c4c932

--- src/cutscene/waitForKey.ts
+++ src/cutscene/waitForKey.ts
@@ -1,17 +1,15 @@
-import {DisplayObject} from "pixi.js";
 import {game} from "../igua/game";
 import {Key, KeyCode} from "../utils/key";
 import {wait} from "../utils/wait";
 import {CancellationToken} from "../utils/cancellablePromise";
+import {invisibleObject} from "../gameObjects/utils/invisibleObject";
 
 export async function waitForKey(keyCode: KeyCode, ct?: CancellationToken)
 {
     let wasUp = false;
     let advance = false;
 
-    const displayObject = new DisplayObject();
-    (displayObject as any).render = () => {};
-    displayObject.withStep(() => {
+    const displayObject = invisibleObject().withStep(() => {
         if (wasUp && Key.justWentDown(keyCode))
             advance = true;
         if (Key.isUp(keyCode))
