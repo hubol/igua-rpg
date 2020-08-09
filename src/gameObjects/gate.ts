fd5b9d23d3d2cc9feee29b0816aadac4a2fcbbfe
Hubol <hubol.gordon@gmail.com>
2020-08-09 15:43:45 -0500

    Experimenting with compile-time resolvers

--- src/gameObjects/gate.ts
+++ src/gameObjects/gate.ts
@@ -6,6 +6,18 @@ import * as levels from "../ogmoLevels";
 import {Test} from "../ogmoLevels";
 import {EscapeTickerAndExecute} from "../utils/iguaTicker";
 import {Gate} from "../sounds";
+import {EntityCommon} from "../igua/level/createGameObjects";
+
+export function resolveGate(entity: EntityCommon)
+{
+    const anyEntity = entity as any;
+
+    const destination = {
+        levelName: anyEntity.levelName,
+        checkpointName: anyEntity.checkpointName,
+    };
+    return game.gameObjectStage.addChild(gate(entity.x, entity.y, entity.width, entity.height, destination));
+}
 
 export function gate(x, y, width, height, destination: { checkpointName, levelName })
 {
