4fcb3678901d9337536fe57e5611a8c87b217919
Hubol <hubol.gordon@gmail.com>
2020-08-10 17:35:17 -0500

    extract findImports

---
+++ cypress/ts-gen/findImports.ts
@@ -0,0 +1,40 @@
+import {Module} from "./module";
+import {ImportedFunction} from "./imported";
+
+export interface Import
+{
+    readonly exportedName: string;
+    readonly modulePath: string;
+}
+
+export function findImports(module: Module)
+{
+    const importJsons = new Set<string>();
+    const next: any[] = [module];
+    while (next.length > 0)
+        findImportsImpl(next.pop(), next, importJsons);
+    const imports = Array.from(importJsons as any).map(x => JSON.parse(x as string) as Import);
+    console.info(module, "Found imports", imports);
+    return imports;
+}
+
+function findImportsImpl(current: any, next: any[], importJsons: Set<string>)
+{
+    if (Array.isArray(current))
+    {
+        current.forEach(x => next.push(x));
+        return;
+    }
+
+    if (typeof current !== "object")
+        return;
+
+    for (const key in current)
+    {
+        const value = current[key];
+        if (value instanceof ImportedFunction)
+            importJsons.add(JSON.stringify(value));
+        else
+            next.push(value);
+    }
+}
--- cypress/ts-gen/writeModule.ts
+++ cypress/ts-gen/writeModule.ts
@@ -1,9 +1,9 @@
 import {Module} from "./module";
-import {ImportedFunction} from "./imported";
 import {getRelativePath} from "pissant-node";
 import {Export} from "./export";
 import {AnonymousFunction} from "./function";
 import {Invocation} from "./invocation";
+import {findImports, Import} from "./findImports";
 
 export function writeModule(module: Module)
 {
@@ -22,7 +22,9 @@ function writeImport(module: Module) {
 
 function writeExport(e: Export)
 {
-    return `export const ${e.member.preferredName} = ${writeLiteral(e.member.value)};`;
+    const value = e.member.value;
+    return writeSuppressingTypeScriptErrorsIfNecessary(value)
+        (`export const ${e.member.preferredName} = ${writeLiteral(value)};`);
 }
 
 function writeLiteral(x: any): string
@@ -31,8 +33,9 @@ function writeLiteral(x: any): string
         return `[ ${x.map(writeLiteral).join(",")} ]`;
     if (x instanceof AnonymousFunction)
     {
+        const value = x.returns.value;
         return `() => {
-    return ${writeLiteral(x.returns.value)};
+${writeSuppressingTypeScriptErrorsIfNecessary(value)(`  return ${writeLiteral(value)};`)}
 }`;
     }
     if (x instanceof Invocation)
@@ -51,52 +54,25 @@ function writeKvPair(object: any)
     return function(key: string)
     {
         const value = object[key];
-        const kvPair = `${key}: ${writeLiteral(value)}`;
-        if (suppressTypeScriptErrors(value))
+        return writeSuppressingTypeScriptErrorsIfNecessary(value)
+            (`${key}: ${writeLiteral(value)}`);
+    }
+}
+
+function writeSuppressingTypeScriptErrorsIfNecessary(mightHaveTypeScriptErrors: any)
+{
+    return function(text: string)
+    {
+        if (suppressTypeScriptErrors(mightHaveTypeScriptErrors))
             return `// @ts-ignore
-${kvPair}`
-        return kvPair;
+${text}`;
+        return text;
     }
 }
 
 function suppressTypeScriptErrors(object: any)
 {
+    if (Array.isArray(object) && object.filter(suppressTypeScriptErrors).length > 0)
+        return true;
     return object instanceof Invocation && object.ignoreProblemsWithInvocation;
-}
-
-interface Import
-{
-    readonly exportedName: string;
-    readonly modulePath: string;
-}
-
-function findImports(module: Module)
-{
-    const imports: Import[] = [];
-    const next: any[] = [module];
-    while (next.length > 0)
-        findImportsImpl(next.pop(), next, imports);
-    console.info(module, "Found imports", imports);
-    return imports;
-}
-
-function findImportsImpl(current: any, next: any[], imports: Import[])
-{
-    if (Array.isArray(current))
-    {
-        current.forEach(x => next.push(x));
-        return;
-    }
-
-    if (typeof current !== "object")
-        return;
-
-    for (const key in current)
-    {
-        const value = current[key];
-        if (value instanceof ImportedFunction)
-            imports.push(value);
-        else
-            next.push(value);
-    }
 }
