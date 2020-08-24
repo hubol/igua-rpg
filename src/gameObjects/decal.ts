6434c6155877e34d3d606758ada63bcfd3914ece
Hubol <hubol.gordon@gmail.com>
2020-08-24 00:55:34 -0500

    amazing decal generation in levelargs

--- src/gameObjects/decal.ts
+++ src/gameObjects/decal.ts
@@ -1,6 +1,8 @@
 import {Sprite, Texture} from "pixi.js";
+import {DecalStyle} from "../../tools/gen-levelargs/types/decalStyle";
+import {game} from "../igua/game";
 
-interface DecalGameObjectArgs
+export interface DecalArgs
 {
     x: number;
     y: number;
@@ -8,14 +10,15 @@ interface DecalGameObjectArgs
     scaleY: number;
     rotation: number;
     texture: Texture;
+    style: DecalStyle;
 }
 
-export function decalGameObject(args: DecalGameObjectArgs)
+export function resolveDecalGameObject(args: DecalArgs)
 {
     const sprite = Sprite.from(args.texture);
     sprite.anchor.set(Math.round(sprite.width / 2) / sprite.width, Math.round(sprite.height / 2) / sprite.height);
     sprite.position.set(args.x, args.y);
     sprite.scale.set(args.scaleX, args.scaleY);
     sprite.rotation = args.rotation;
-    return sprite;
+    return game.gameObjectStage.addChild(sprite);
 }
