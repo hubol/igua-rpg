a54da1199d5cd858fda6bbf977616479f8edddc9
Hubol <hubol.gordon@gmail.com>
2020-09-11 11:26:02 -0500

    Add valuables to hud

--- src/igua/createDefaultHud.ts
+++ src/igua/createDefaultHud.ts
@@ -1,22 +1,27 @@
-import {Container, Graphics} from "pixi.js";
+import {BitmapText, Container, Graphics} from "pixi.js";
 import {progress} from "./progress";
 import {game} from "./game";
+import {AcrobatixFont} from "../fonts";
 
 export function createDefaultHud()
 {
-    const healthbar = new Graphics();
-    healthbar.withStep(() => {
-        healthbar.clear();
-        healthbar.beginFill(0xff0000);
-        healthbar.drawRect(0, 0, progress.maxHealth, 16);
-        healthbar.beginFill(0x0000ff);
-        healthbar.drawRect(0, 0, progress.health, 16);
-    });
+    const healthbar = new Graphics()
+        .withStep(() => {
+            healthbar.clear();
+            healthbar.beginFill(0xff0000);
+            healthbar.drawRect(0, 0, progress.maxHealth, 16);
+            healthbar.beginFill(0x0000ff);
+            healthbar.drawRect(0, 0, progress.health, 16);
+        });
+
+    const valuables = new BitmapText("", { fontName: AcrobatixFont.font, tint: 0x00ff00 })
+        .withStep(() => valuables.text = `${progress.valuables} valuables`)
+        .at(2, 15);
 
     const defaultHud = new Container()
         .withStep(() => defaultHud.visible = !game.player.isDead);
 
-    defaultHud.addChild(healthbar);
+    defaultHud.addChild(healthbar, valuables);
 
     game.hudStage.addChild(defaultHud);
 }
