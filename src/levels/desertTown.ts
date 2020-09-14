b3fbf45954a8620011685e699820389e6ab05eb7
Hubol <hubol.gordon@gmail.com>
2020-09-14 16:11:24 -0500

    Add bugged sign cutscene to test music change
    Former-commit-id: 8b7f63494286f0f5e605cb67da8b4ba5314b1300
    Former-commit-id: 8f78da76d1b55bddf261cb6e84f4461a2881c53e

--- src/levels/desertTown.ts
+++ src/levels/desertTown.ts
@@ -1,7 +1,7 @@
 import {applyOgmoLevel} from "../igua/level/applyOgmoLevel";
 import {DesertTownArgs} from "../levelArgs";
 import {jukebox} from "../igua/jukebox";
-import {Country} from "../musics";
+import {Country, Oracle} from "../musics";
 
 export function DesertTown()
 {
@@ -11,4 +11,5 @@ export function DesertTown()
     level.LeftHouse.tint = 0xA0C0C0;
     level.LeftHouseDoor.locked = true;
     level.RightHouseDoor.locked = true;
+    level.Sign.cutscene = async () => jukebox.play(Oracle);
 }
