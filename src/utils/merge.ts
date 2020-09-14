f490bda5313bd18e63642d0cf9c1da91ef896d18
Hubol <hubol.gordon@gmail.com>
2020-09-14 16:07:31 -0500

    Do not implement your own Object.assign -_-
    Former-commit-id: 2e12ab4e2d75d4ccbdd43344c1fe65cf20f8ae37
    Former-commit-id: 2eadd2d69bc93ede313f24a3d2adf19b6e6382f9

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,15 +1,4 @@
-// https://stackoverflow.com/a/50270314
-
-function hasOwn(obj, prop)
+export function merge<T, U>(target: T, source: U): T & U
 {
-    return Object.prototype.hasOwnProperty.call(obj, prop);
-}
-
-export function merge<T, U>(target: T, source: U): T & U {
-    for (const key in source) {
-        if (hasOwn(source, key)) {
-            Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key) as any);
-        }
-    }
-    return target as T & U;
+    return Object.assign(target, source);
 }
