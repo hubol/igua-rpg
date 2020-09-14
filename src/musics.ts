eac20cf1592b6350cc99c6360d8e539e9ad624fa
Hubol <hubol.gordon@gmail.com>
2020-09-14 15:13:50 -0500

    Remove eggworld.wav and generate musics.ts
    Former-commit-id: ea71e967452d234fb283b9d38e714f2ad339eb2d
    Former-commit-id: 68a677482d7a522296ea66a8fe095be52b3c6d50

--- src/musics.ts
+++ src/musics.ts
@@ -2,7 +2,17 @@ import { Howl } from 'howler';
     
 // This file is generated. Do not touch.
 
-export const Eggworld = new Howl({
-    src: [require("./music/eggworld.ogg"), require("./music/eggworld.mp3")],
+export const Country = new Howl({
+    src: [require("./music/country.ogg"), require("./music/country.mp3")],
+    preload: false
+});
+
+export const DesertTown = new Howl({
+    src: [require("./music/desert town.ogg"), require("./music/desert town.mp3")],
+    preload: false
+});
+
+export const Oracle = new Howl({
+    src: [require("./music/oracle.ogg"), require("./music/oracle.mp3")],
     preload: false
 });
