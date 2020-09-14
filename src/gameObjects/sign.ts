26c3af11a3730c2675af82a225369ff9e9b11bdc
Hubol <hubol.gordon@gmail.com>
2020-09-14 16:09:58 -0500

    Can now override cutscene in a sign

--- src/gameObjects/sign.ts
+++ src/gameObjects/sign.ts
@@ -5,21 +5,24 @@ import {AtomixFont} from "../fonts";
 import {isPlayerInteractingWith} from "../igua/isPlayerInteractingWith";
 import {game} from "../igua/game";
 import {resolveGameObject} from "../../tools/gen-levelargs/resolveGameObject";
+import {Cutscene} from "../cutscene/cutscene";
+import {merge} from "../utils/merge";
 
 export const resolveSign =
     resolveGameObject("Sign", e => game.gameObjectStage.addChild(sign(e, (e as any).title, (e as any).message)));
 
 export function sign(vector: Vector, title: string, message: string)
 {
+    const cutscene: Cutscene = async p => await p.show(message);
     const sprite = new Sprite(Sign);
     const text = new BitmapText(title, { fontName: AtomixFont.font, tint: 0xA08030 }).at(1, -2);
 
-    const container = new Container();
+    const container = merge(new Container(), { cutscene });
     container.addChild(sprite, text);
     return container
         .at(vector.x - 14, vector.y - 18)
         .withStep(() => {
             if (isPlayerInteractingWith(container))
-                game.cutscenePlayer.playCutscene(async p => await p.show(message));
+                game.cutscenePlayer.playCutscene(container.cutscene);
         });
 }
