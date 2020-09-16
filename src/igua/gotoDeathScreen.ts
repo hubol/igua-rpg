53f7c495d979fefde588fe2210a702bf47235afc
Hubol <hubol.gordon@gmail.com>
2020-09-16 15:58:30 -0500

    Music stops on the death screen

--- src/igua/gotoDeathScreen.ts
+++ src/igua/gotoDeathScreen.ts
@@ -1,9 +1,11 @@
 import {level} from "./level/level";
 import {game, loadSavedProgress, recreatePlayer} from "./game";
 import {progress} from "./progress";
+import {jukebox} from "./jukebox";
 
 export function gotoDeathScreen()
 {
+    jukebox.stop();
     level.clear();
     game.player.isDead = true;
     game.backgroundColor = 0x000000;
