a46280e1291c0018aa894179300f4f2b74dd4b92
Hubol <hubol.gordon@gmail.com>
2020-09-16 12:02:07 -0500

    more cleanup

--- src/utils/createApplication.ts
+++ src/utils/createApplication.ts
@@ -2,19 +2,17 @@ import {Application} from "pixi.js";
 
 type ApplicationOptions = ConstructorParameters<typeof Application>[0] & { targetFps: number };
 
-export function startApplication(options: ApplicationOptions)
+export function createApplication(options: ApplicationOptions)
 {
     const app = new Application(options);
 
     if (options.targetFps)
         app.ticker.maxFPS = options.targetFps;
 
-    addApplicationToDocument(app);
-    return app;
-}
-
-function addApplicationToDocument(app)
-{
-    app.view.id = "gameCanvas";
-    document.body.appendChild(app.view);
+    return {
+        canvasElement: app.view,
+        renderer: app.renderer,
+        stage: app.stage,
+        ticker: app.ticker
+    };
 }
