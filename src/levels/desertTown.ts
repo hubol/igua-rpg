fef483d5f42b1b5a9387ba0cd252068c71481880
Hubol <hubol.gordon@gmail.com>
2020-09-26 18:24:50 -0500

    terrain and background color is not set using level args

--- src/levels/desertTown.ts
+++ src/levels/desertTown.ts
@@ -2,11 +2,15 @@ import {applyOgmoLevel} from "../igua/level/applyOgmoLevel";
 import {DesertTownArgs} from "../levelArgs";
 import {jukebox} from "../igua/jukebox";
 import {Country, Oracle} from "../musics";
+import {game} from "../igua/game";
 
 export function DesertTown()
 {
     jukebox.play(Country).warm(Oracle);
     const level = applyOgmoLevel(DesertTownArgs);
+    game.backgroundColor = 0xF0F0B0;
+    game.terrainColor = 0xE0D060;
+
     level.RightHouse.tint = 0xE08060;
     level.LeftHouse.tint = 0xA0C0C0;
     level.RightHouseDoor.locked = true;
