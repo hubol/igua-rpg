6fde6befa4463bbc9500efc9ad6fe0703ac9ba97
Hubol <hubol.gordon@gmail.com>
2020-09-30 13:39:27 -0500

    Add naive progress flags

---
+++ src/igua/flags.ts
@@ -0,0 +1,7 @@
+export function getInitialFlags()
+{
+    return {
+        stackedAllCrates: false,
+        thankedByCrateStacker: false,
+    };
+}
--- src/levels/desertTown.ts
+++ src/levels/desertTown.ts
@@ -7,6 +7,7 @@ import {Sprite} from "pixi.js";
 import {CrateWooden} from "../textures";
 import {isOnScreen} from "../igua/isOnScreen";
 import { CratePickup, CratePlace } from "../sounds";
+import {progress} from "../igua/progress";
 
 function getDesertTownLevel()
 {
@@ -32,61 +33,87 @@ export function DesertTown()
 function enhanceCrateStacker(level: DesertTownLevel)
 {
     const crates = [ level.Crate0, level.Crate1, level.Crate2, level.Crate3, level.Crate4, level.Crate5, level.Crate6, level.Crate7, level.Crate8, level.Crate9 ];
+    let cratesLeftToStack = crates.length;
 
     let lastStackedCrate: Sprite | null;
-    function stackCrate()
+    function stackCrate(playSound = true)
     {
+        cratesLeftToStack--;
+        if (cratesLeftToStack <= 0)
+            progress.flags.stackedAllCrates = true;
+
         const nextCrate = Sprite.from(CrateWooden).at(
             lastStackedCrate
                 ? { x: lastStackedCrate.x + Math.round(-6 + Math.random() * 12), y: lastStackedCrate.y - lastStackedCrate.height }
                 : level.DropCrateAnchor);
         game.backgroundGameObjectStage.addChild(nextCrate);
         lastStackedCrate = nextCrate;
-        if (isOnScreen(level.DropCrateAnchor))
+        if (playSound && isOnScreen(level.DropCrateAnchor))
             CratePlace.play();
     }
 
-    function pickupCrate()
+    function pickupCrate(playSound = true)
     {
         const crate = crates.shift();
         if (!crate)
             return false;
 
-        if (isOnScreen(level.PickupCratesRegion))
+        if (playSound && isOnScreen(level.PickupCratesRegion))
             CratePickup.play();
         crate.destroy();
         return true;
     }
 
-    let tiredOfWorking = false;
+    if (progress.flags.stackedAllCrates)
+    {
+        while (pickupCrate(false))
+            stackCrate(false);
+
+        level.Stacker.at(896, 296);
+        level.Stacker.duckUnit = 1;
+        level.Stacker.isDucking = true;
+    }
+
+    let tiredOfWorking = progress.flags.stackedAllCrates;
 
     level.Stacker.cutscene = async p => {
-        if (tiredOfWorking)
+        if (progress.flags.stackedAllCrates && !progress.flags.thankedByCrateStacker)
+        {
+            await p.show("Thank you for your help.");
+            await p.show("All I have to show my appreciation is this old key.");
+            await p.show("Received the key.");
+            progress.flags.thankedByCrateStacker = true;
+        }
+        else if (tiredOfWorking)
             await p.show("I think I'm done working for today.");
         else
             await p.show("It's my job to take the crates and stack them.");
     };
     level.Stacker.engine.walkSpeed = 1;
-    level.Stacker.withAsync(async p => {
-        await p.wait(() => isOnScreen(level.Stacker));
-        while (crates.length > 2)
-        {
-            await level.Stacker.walkTo(crates[0].x);
-            await p.sleep(500);
-            if (!pickupCrate())
-                continue;
 
-            await p.sleep(500);
-            await level.Stacker.walkTo(level.DropCrateAnchor.x + 32);
-            await p.sleep(500);
-            stackCrate();
-            await p.sleep(500);
-        }
+    if (!tiredOfWorking)
+    {
+        level.Stacker.withAsync(async p => {
+            await p.wait(() => isOnScreen(level.Stacker));
+            while (crates.length > 2)
+            {
+                await level.Stacker.walkTo(crates[0].x);
+                await p.sleep(500);
+                if (!pickupCrate())
+                    continue;
 
-        await level.Stacker.walkTo(level.DropCrateAnchor.x + 96);
-        level.Stacker.isDucking = true;
-        tiredOfWorking = true;
-    });
+                await p.sleep(500);
+                await level.Stacker.walkTo(level.DropCrateAnchor.x + 32);
+                await p.sleep(500);
+                stackCrate();
+                await p.sleep(500);
+            }
+
+            await level.Stacker.walkTo(level.DropCrateAnchor.x + 96);
+            level.Stacker.isDucking = true;
+            tiredOfWorking = true;
+        });
+    }
 
     let playerHasCrate = false;
 
