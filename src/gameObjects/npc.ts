fd10fcfa9163fabcdded36554e6569fb6dcebbe3
Hubol <hubol.gordon@gmail.com>
2020-09-30 21:46:32 -0500

    extract npcStyles
    Former-commit-id: 736dc0aaf8e4fbb7fe4715d5aa7a22646c124d93
    Former-commit-id: 099b4db626deafd3b09a3ca4017c36c824c3565e

--- src/gameObjects/npc.ts
+++ src/gameObjects/npc.ts
@@ -1,19 +1,4 @@
-import {Container, Sprite, DisplayObject} from "pixi.js";
-import {
-    CharacterHead,
-    CharacterMouthV,
-    CharacterPupils,
-    CharacterWhites, NpcBlueFoot, NpcBlueFootRear,
-    NpcCurvedCrest,
-    NpcCurvedCrest2,
-    NpcGreenPolkaBody, NpcPupilsAngry,
-    NpcRedFoot,
-    NpcRedFootRear,
-    NpcWeirdBody
-} from "../textures";
-import {IguanaPuppet, iguanaPuppet} from "../igua/iguanaPuppet";
 import {game} from "../igua/game";
-import {IguanaEyes, iguanaEyes} from "../igua/iguanaEyes";
 import {resolveGameObject} from "../../tools/gen-levelargs/resolveGameObject";
 import {Cutscene} from "../cutscene/cutscene";
 import {merge} from "../utils/merge";
@@ -21,6 +6,7 @@ import {isPlayerInteractingWith} from "../igua/isPlayerInteractingWith";
 import {makeIguanaEngine} from "../igua/puppet/makeIguanaEngine";
 import {rejection} from "../utils/rejection";
 import {NpcMod} from "./npcMods";
+import {getNpcStyle} from "./npcStyles";
 
 export const resolveNpc = resolveGameObject("NpcIguana", e => {
     const n = game.gameObjectStage.addChild(npc(e.x, e.y - 8, (e as any).style));
@@ -66,7 +52,7 @@ function makeNpcMods(npc: Npc)
 
 function makeCutscenePuppet(style: number)
 {
-    const puppet = npcStyles[style]();
+    const puppet = getNpcStyle(style)();
     return merge(puppet, {
         cutscene: undefined as Cutscene | undefined,
         get isCutscenePlaying() {
@@ -159,109 +145,4 @@ function makeDriver(puppet: ReturnType<typeof makeCutscenePuppet>)
                 next.push({ x, resolve, reject: () => reject(rejection("walkTo Promise rejected due to iguana puppet DisplayObject removal")) }))
         }
     };
-}
-
-let npcStyles: Array<() => IguanaPuppet> = [];
-
-npcStyles[0] = npcStyle(args => {
-    args.body = Sprite.from(NpcWeirdBody);
-    args.body.pivot.y += 4;
-    args.body.tint = 0xFF8A7C;
-
-    args.frontRightFoot = Sprite.from(NpcRedFoot);
-
-    args.backRightFoot = Sprite.from(NpcRedFootRear);
-    args.backRightFoot.pivot.y = 1;
-
-    args.crest = Sprite.from(NpcCurvedCrest);
-    args.crest.pivot.set(-2, 2);
-    args.crest.tint = 0x75CAFF;
-
-    args.headSprite.tint = 0x73C88C;
-    args.mouthSprite.tint = 0xA83F2F;
-
-    args.pupils.tint = 0xA83F2F;
-    args.eyelidColor = 0x5AA86E;
-});
-
-npcStyles[1] = npcStyle(args => {
-    args.body = Sprite.from(NpcGreenPolkaBody);
-    args.body.tint = 0xD8D44E;
-    args.body.pivot.y += 3;
-
-    args.frontRightFoot = Sprite.from(NpcBlueFoot);
-    args.frontRightFoot.pivot.y = 2;
-
-    args.backRightFoot = Sprite.from(NpcBlueFootRear);
-    args.backRightFoot.pivot.y = 2;
-
-    args.crest = Sprite.from(NpcCurvedCrest2);
-    args.crest.pivot.set(-2, 2);
-    args.crest.tint = 0x198A96;
-
-    args.headSprite.tint = 0xFF8E7F;
-    args.mouthSprite.tint = 0x2A87A9;
-
-    args.pupils = Sprite.from(NpcPupilsAngry);
-    args.pupils.pivot.y -= 1;
-    args.pupils.tint = 0x2A87A9;
-    args.eyelidColor = 0xA9272C;
-});
-
-function npcStyle(configure: (args: ConfigureNpcStyleArgs) => void)
-{
-    return () => {
-
-        const pupils = Sprite.from(CharacterPupils);
-        pupils.pivot.set(-2, -3);
-
-        const mouthSprite = Sprite.from(CharacterMouthV);
-        mouthSprite.pivot.set(-10, -11);
-
-        const args = {headSprite: Sprite.from(CharacterHead), mouthSprite, pupils, eyeShape: Sprite.from(CharacterWhites)} as unknown as ConfigureNpcStyleArgs;
-        configure(args);
-
-        if (!args.frontLeftFoot)
-        {
-            args.frontLeftFoot = Sprite.from(args.frontRightFoot.texture);
-            args.frontLeftFoot.tint = 0xC7D7D7;
-            args.frontLeftFoot.pivot.copyFrom(args.frontRightFoot.pivot);
-        }
-
-        if (!args.backLeftFoot)
-        {
-            args.backLeftFoot = Sprite.from(args.backRightFoot.texture);
-            args.backLeftFoot.tint = 0xC7D7D7;
-            args.backLeftFoot.pivot.copyFrom(args.backRightFoot.pivot);
-        }
-
-        if (!args.head)
-        {
-            const container = new Container();
-            args.head = container;
-            container.addChild(args.headSprite, args.mouthSprite);
-        }
-
-        if (!args.eyes)
-            args.eyes = iguanaEyes(args as any);
-
-        return iguanaPuppet(args as any);
-    }
-}
-
-interface ConfigureNpcStyleArgs
-{
-    body: Sprite;
-    backLeftFoot?: Sprite;
-    backRightFoot: Sprite;
-    frontLeftFoot?: Sprite;
-    frontRightFoot: Sprite;
-    head?: DisplayObject;
-    headSprite: Sprite;
-    mouthSprite: Sprite;
-    crest: Sprite;
-    eyes?: IguanaEyes;
-    pupils: Sprite;
-    eyeShape: Container;
-    eyelidColor?: number;
 }
