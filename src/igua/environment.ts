353631138ad71c10f4f8e88d828e3ad3300972f9
Hubol <hubol.gordon@gmail.com>
2020-10-03 15:18:59 -0500

    Cherry-pick changes from feature/electron

--- src/igua/environment.ts
+++ src/igua/environment.ts
@@ -3,6 +3,11 @@ export const environment = {
     {
         return process.env.NODE_ENV === "production";
     },
+    get isElectron()
+    {
+        // https://github.com/electron/electron/issues/2288#issuecomment-337858978
+        return navigator.userAgent.toLowerCase().indexOf(' electron/') > -1;
+    },
     get isSafari()
     {
         return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
