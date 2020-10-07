63ca87cac2c91bcd97dbc87cdc718acf71a113ca
Hubol <hubol.gordon@gmail.com>
2020-10-07 12:31:00 -0500

    canvas sink starts black. support ios PWA for some reason haha

--- src/utils/make2dCanvasSink.ts
+++ src/utils/make2dCanvasSink.ts
@@ -3,9 +3,11 @@ export function make2dCanvasSink(canvasElement: HTMLCanvasElement)
     console.log("Using 2d canvas sink");
 
     const clonedCanvasElement = document.createElement("canvas");
-    clonedCanvasElement.width = canvasElement.width;
-    clonedCanvasElement.height = canvasElement.height;
+    const { width, height } = canvasElement;
+    clonedCanvasElement.width = width;
+    clonedCanvasElement.height = height;
     const context = clonedCanvasElement.getContext("2d");
+    context?.fillRect(0, 0, width, height);
 
     function copyToCanvas()
     {
