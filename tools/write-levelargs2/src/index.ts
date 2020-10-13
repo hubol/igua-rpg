f44ca2f919250b89c37ee9d211be355ed0d93563
Hubol <hubol.gordon@gmail.com>
2020-10-13 13:14:10 -0500

    Use new write-levelargs from package scripts
    Former-commit-id: c03ec62e594923c15dc4f28493a41e42d6ffde3b
    Former-commit-id: 090672a8736ab2bac3dd3ed384ae91790c77b32d

--- tools/write-levelargs2/src/index.ts
+++ tools/write-levelargs2/src/index.ts
@@ -4,30 +4,17 @@ import {waitForGlobal} from "./waitForGlobal";
 import {GameObjectResolver} from "../../gen-levelargs/types/gameObjectResolver";
 import {writeLevelArgsFile} from "./writeLevelArgsFile";
 
-let browserWindow;
-
 async function createWindow()
 {
-    browserWindow = new BrowserWindow({ show: false });
+    const browserWindow = new BrowserWindow({ show: false });
 
-    browserWindow.loadURL(`file://${join(__dirname, ".app/index.html")}`);
+    await browserWindow.loadURL(`file://${join(__dirname, ".app/index.html")}`);
     const gameObjectResolvers =
         await waitForGlobal<GameObjectResolver[]>(browserWindow)("window.__gameObjectResolvers");
 
     await writeLevelArgsFile({ levelArgsFilePath: "../../src/levelArgs.ts", gameObjectResolvers, ogmoLevelsDirectoryPath: "../../raw/ogmo/levels" });
 
-    browserWindow.on("closed", () => (browserWindow = null));
     browserWindow.close();
 }
 
 app.on("ready", createWindow);
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin")
-        app.quit();
-});
-
-app.on("activate", () => {
-    if (browserWindow === null)
-        createWindow();
-});
