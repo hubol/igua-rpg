b7acb6b59bea4fbe9e7f427e813f81aeda1fe129
Hubol <hubol.gordon@gmail.com>
2020-10-22 18:47:11 -0500

    Player is recreated on each level

--- src/igua/level/level.ts
+++ src/igua/level/level.ts
@@ -1,12 +1,14 @@
 import {getLevelApplicator, LevelApplicator} from "./getLevelApplicator";
 import {progress} from "../data/progress";
 import {sceneStack} from "../scene";
+import {recreatePlayer} from "../game";
 
 export const level = {
     goto(levelName: string)
     {
         sceneStack.pop();
         sceneStack.push();
+        recreatePlayer();
         this.current = getLevelApplicator(levelName);
         this.current();
         progress.levelName = levelName;
