64781bd69251bd5c25be0c319c28f3a330427c9a
Hubol <hubol.gordon@gmail.com>
2020-11-03 21:31:24 -0600

    Add digua join scene
    Former-commit-id: 35aa90eb03b8477c72611b1410f25e2341816d6e
    Former-commit-id: f658655b9176ee8130a8315603809e196b701ec7

--- src/levels/desertField.ts
+++ src/levels/desertField.ts
@@ -4,6 +4,8 @@ import {scene} from "../igua/scene";
 import {applyOgmoLevel} from "../igua/level/applyOgmoLevel";
 import {DesertFieldArgs} from "../levelArgs";
 import {Sleepy} from "../igua/puppet/mods/sleepy";
+import {createFollower, player} from "../gameObjects/player";
+import {progress} from "../igua/data/progress";
 
 export function DesertField()
 {
@@ -17,4 +19,36 @@ export function DesertField()
     level.Digua.mods.add(Sleepy);
     level.Digua.duckImmediately();
     level.Digua.closeEyesImmediately();
+
+    let rejectedOnce = false;
+
+    level.Digua.cutscene = async p => {
+        level.Digua.mods.remove(Sleepy);
+        await p.sleep(1000);
+        if (rejectedOnce)
+        {
+            await p.show("Oh. It's you again.");
+        }
+        else
+        {
+            await p.show("Oh. I guess I fell asleep here when I realized it was unsafe to cross the desert.");
+            await p.show("I'm Digua. My talent is digging things out of the ground.");
+        }
+
+        if (!await p.ask("Can I follow you around for a bit?"))
+        {
+            rejectedOnce = true;
+            await p.show("Ok. I'll be here if you change your mind.")
+            level.Digua.mods.add(Sleepy);
+            return;
+        }
+
+        level.Digua.isDucking = false;
+        await p.sleep(500);
+        await p.show("Awesome! Let's go on an adventure!");
+
+        progress.flags.diguaIsFollowing = true;
+        player.follower = createFollower();
+        level.Digua.destroy();
+    };
 }
