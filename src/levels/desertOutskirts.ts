fbe6d9b0eff3cb91f7d9736c599e87147317012a
Hubol <hubol.gordon@gmail.com>
2020-11-13 14:56:10 -0600

    Semi-working unlock temple cutscene
    Former-commit-id: 1264afea3e587035c70ad2fd73cd116d06c79cbb
    Former-commit-id: 0444b7992206795e2c7f32e1ad5a23c8174092f9

--- src/levels/desertOutskirts.ts
+++ src/levels/desertOutskirts.ts
@@ -1,7 +1,9 @@
 import {applyOgmoLevel} from "../igua/level/applyOgmoLevel";
 import {DesertOutskirtsArgs} from "../levelArgs";
-import {scene} from "../igua/scene";
+import {scene, sceneStack} from "../igua/scene";
 import {progress} from "../igua/data/progress";
+import {DesertField} from "./desertField";
+import {game} from "../igua/game";
 
 function getDesertOutskirtsLevel()
 {
@@ -22,6 +24,20 @@ export function DesertOutskirts()
 function enrichUnlockTemple(level: DesertOutskirtsLevel)
 {
     level.TempleUnlockBlob.withInteraction(() => {
-        progress.flags.desert.unlockedTemple = true;
+        game.cutscenePlayer.playCutscene(async p => {
+            sceneStack.push();
+            const level = DesertField();
+            scene.camera.x = 1296;
+            const goalX = Math.round(level.TempleDoor.x - 128);
+            const goalY = Math.round(level.TempleDoor.y - 128);
+            scene.camera.y = goalY;
+            await p.move(scene.camera).to(goalX, goalY).over(5_000);
+            await p.sleep(500);
+            // TODO sfx
+            level.TempleDoor.locked = false;
+            await p.sleep(500);
+            sceneStack.pop();
+            progress.flags.desert.unlockedTemple = true;
+        })
     });
 }
