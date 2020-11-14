25cb2d714c411bae57f42daa8b10478eb23f8645
Hubol <hubol.gordon@gmail.com>
2020-11-14 11:13:29 -0600

    Add lerp to promiseLibrary

---
+++ src/gameObjects/flash.ts
@@ -0,0 +1,16 @@
+import {Graphics} from "pixi.js";
+import {merge} from "../utils/merge";
+import {game} from "../igua/game";
+
+export function flash(color: number, alpha = 1)
+{
+    const graphics = merge(new Graphics(), { alpha });
+    graphics.withStep(() => {
+        graphics.clear();
+        graphics.beginFill(color, graphics.alpha);
+        graphics.drawRect(0, 0, 256, 256);
+    });
+
+    game.hudStage.addChild(graphics);
+    return graphics;
+}
