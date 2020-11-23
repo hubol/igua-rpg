a37869c03318816c46908815bdbc15cab01c1211
Hubol <hubol.gordon@gmail.com>
2020-11-23 12:54:05 -0600

    array prototype pollutions are no longer enumerable

--- src/utils/extensions/arrayExtensions.ts
+++ src/utils/extensions/arrayExtensions.ts
@@ -6,30 +6,38 @@ declare global {
     }
 }
 
-Array.prototype.removeFirst = function(...args)
-{
-    args.forEach(value => {
-        const index = this.indexOf(value);
-        if (index > -1)
-            this.splice(index, 1);
-    });
-};
-
-Array.prototype.removeAll = function(value)
-{
-    while (true)
-    {
-        const index = this.indexOf(value);
-        if (index <= -1)
-            return;
-        this.splice(index, 1);
+Object.defineProperties(Array.prototype, {
+    removeFirst: {
+        value: function (...args) {
+            args.forEach(value => {
+                const index = this.indexOf(value);
+                if (index > -1)
+                    this.splice(index, 1);
+            });
+        },
+        enumerable: false
+    },
+    removeAll: {
+        value: function(value)
+        {
+            while (true)
+            {
+                const index = this.indexOf(value);
+                if (index <= -1)
+                    return;
+                this.splice(index, 1);
+            }
+        },
+        enumerable: false
+    },
+    firstOrDefault: {
+        value: function () {
+            if (this.length === 0)
+                return undefined;
+            return this[0];
+        },
+        enumerable: false
     }
-};
-
-Array.prototype.firstOrDefault = function () {
-    if (this.length === 0)
-        return undefined;
-    return this[0];
-}
+});
 
 export default 0;
