414c5090c11487ef9164bddebf99f49d6eb2314f
Hubol <hubol.gordon@gmail.com>
2020-11-26 23:43:52 -0600

    Add big key piece to mimic
    Former-commit-id: 1e72de4c12a5a738640cb7c7067f77e494bde725
    Former-commit-id: 4e531cd520f99b74e6073e543d66bf1603a613b3

--- src/levels/unrealMimic.ts
+++ src/levels/unrealMimic.ts
@@ -3,6 +3,10 @@ import {UnrealMimicArgs} from "../levelArgs";
 import {portalFluidConfig} from "../gameObjects/portalFluid";
 import {scene} from "../igua/scene";
 import { filters } from "pixi.js";
+import {player} from "../gameObjects/player";
+import {bigKeyPiece} from "../gameObjects/bigKey";
+import {progress} from "../igua/data/progress";
+import {desertBigKeyTextures} from "./desertTemple";
 
 export function UnrealMimic()
 {
@@ -16,4 +20,25 @@ export function UnrealMimic()
     colorMatrixFilter.matrix = [0, 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0, 0, 0, 0, .875, 0, 0, 0, 0, 1, 0];
     level.Mimic.filters = [colorMatrixFilter];
     level.Mimic.x -= 0.1; // TODO this is very messed up
+
+    scene.backgroundGameObjectStage.addChild(
+        bigKeyPiece(progress.flags.desert.bigKey, desertBigKeyTextures[1], "piece2")
+            .at(level.BigKeyPiece)
+    );
+
+    function defeatMimic()
+    {
+        level.PlayerFloorBlock.destroy();
+        if (player.vspeed === 0) // TODO this is a hack (player does not automatically fall when solid below it is destroyed)
+            player.vspeed = 1;
+        scene.gameObjectStage.withAsync(async p => {
+            await Promise.all([
+                p.lerp(level.Mimic.scale, "x").to(0).over(1000),
+                p.lerp(level.Mimic.scale, "y").to(0).over(1000),
+                p.lerp(level.Mimic, "angle").to(720).over(1000)
+                ]);
+        });
+    }
+
+    setTimeout(defeatMimic, 1000);
 }
