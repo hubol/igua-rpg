0e992c01765fa45408cfec4735aa1539dec40c76
Hubol <hubol.gordon@gmail.com>
2020-12-01 18:50:10 -0600

    fix upscale algorithm

--- src/igua/upscaleGameCanvas.ts
+++ src/igua/upscaleGameCanvas.ts
@@ -8,7 +8,7 @@ export function upscaleGameCanvas(canvasElement: HTMLCanvasElement)
     onViewportResize(doUpscale);
 }
 
-function makeDoUpscale(element: HTMLCanvasElement)
+function makeDoUpscale(canvas: HTMLCanvasElement)
 {
     let lastSeenViewportMin = -1;
 
@@ -17,11 +17,31 @@ function makeDoUpscale(element: HTMLCanvasElement)
         if (viewport.min === lastSeenViewportMin)
             return;
 
-        const width = Math.max(1, Math.floor(viewport.min / element.width - .125)) * element.width;
-        const height = Math.max(1, Math.floor(viewport.min / element.height - .125)) * element.height;
+        const padding = viewport.min * 0.125;
+        const availableWidth = viewport.width - padding;
+        const availableHeight = viewport.height - padding;
 
-        element.style.width = `${width}px`;
-        element.style.height = `${height}px`;
+        const availableAspectRatio = availableWidth / availableHeight;
+        const naturalAspectRatio = canvas.width / canvas.height;
+
+        const linearScale = availableAspectRatio < naturalAspectRatio
+            ? (availableWidth / canvas.width)
+            : (availableHeight / canvas.height);
+
+        const scale = getSteppedScale(linearScale);
+
+        const width = scale * canvas.width;
+        const height = scale * canvas.height;
+
+        canvas.style.width = `${width}px`;
+        canvas.style.height = `${height}px`;
         lastSeenViewportMin = viewport.min;
     }
+}
+
+function getSteppedScale(linearScale: number)
+{
+    if (window.devicePixelRatio > 0)
+        return Math.max(1, Math.floor(linearScale * window.devicePixelRatio)) / window.devicePixelRatio;
+    return Math.max(1, Math.floor(linearScale));
 }
