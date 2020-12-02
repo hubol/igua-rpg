0f91650435b3389b849785e740728442e803c3dd
Hubol <hubol.gordon@gmail.com>
2020-12-01 21:55:38 -0600

    Add new sounds to temple unlock
    Former-commit-id: 6ba6bb930fbb86909002743f7c027ded27968d03
    Former-commit-id: 9f2ee1399251c68a6022bbbd49350628cf638dea

--- src/igua/dev/devProgress.ts
+++ src/igua/dev/devProgress.ts
@@ -4,11 +4,11 @@ export function devProgress()
 {
     progress.flags.desert.heardIntroduction = true;
     progress.flags.desert.stackedAllCrates = true;
-    progress.flags.desert.unlockedTemple = true;
+    // progress.flags.desert.unlockedTemple = true;
     progress.flags.desert.key.fromCrateStacker = true;
     progress.flags.desert.key.fromInn = true;
     // progress.flags.desert.diguaIsFollowing = true;
     progress.valuables = 100;
-    progress.levelName = "UnrealMimic";
+    progress.levelName = "DesertOutskirts";
     // progress.flags.diguaIsFollowing = true;
 }
--- src/levels/desertOutskirts.ts
+++ src/levels/desertOutskirts.ts
@@ -9,6 +9,9 @@ import {subimageTextures} from "../utils/pixi/simpleSpritesheet";
 import {DesertTempleLever} from "../textures";
 import {add} from "../utils/math/vector";
 import {cutscene} from "../cutscene/cutscene";
+import {ActivateLever, TempleDoorOpen, TransitionSlide} from "../sounds";
+import {ShockwaveFilter} from "@pixi/filter-shockwave";
+import {game} from "../igua/game";
 
 function getDesertOutskirtsLevel()
 {
@@ -47,9 +50,12 @@ function enrichUnlockTemple(level: DesertOutskirtsLevel)
         }
 
         cutscene.play(async p => {
+            ActivateLever.play();
             await p.lerp(leverObject, "angle").to(onAngle).over(200);
 
             const flashObject = flash(0xF0F0B0, 0);
+            TransitionSlide.volume(0.7);
+            TransitionSlide.play();
             await p.lerp(flashObject, "alpha").to(1).over(500);
 
             sceneStack.push();
@@ -64,9 +70,14 @@ function enrichUnlockTemple(level: DesertOutskirtsLevel)
 
             await p.move(scene.camera).to(goalX, goalY).over(4_000);
             await p.sleep(500);
-            // TODO sfx
+
+            const shockwaveFilter = new ShockwaveFilter([128, 136]);
+            game.sceneStage.filters = [shockwaveFilter];
             level.TempleDoor.locked = false;
+            TempleDoorOpen.play();
+            await p.lerp(shockwaveFilter, "time").to(1).over(1000);
             await p.sleep(500);
+            game.sceneStage.filters = [];
             sceneStack.pop();
             progress.flags.desert.unlockedTemple = true;
         })
