12d6689850f4292b35030d37a248069763c51fd5
Hubol <hubol.gordon@gmail.com>
2021-08-30 19:03:58 -0500

    Not very good snowman
    Former-commit-id: f239cd6e81b4e5721ebff4258701408e946a9717
    Former-commit-id: 2f3d0f85e4edac4abb741f2283fe957f74331321

--- src/levelArgs.ts
+++ src/levelArgs.ts
@@ -1756,7 +1756,8 @@ Block_3: resolveBlock({"type":"Block","x":224,"y":72,"width":32,"height":64,"fli
 TorchA: {"type":"Anchor","x":16,"y":136,"flippedX":false,"flippedY":false,"uid":"25979726","name":"TorchA"},
 Torch1: {"type":"Anchor","x":280,"y":224,"flippedX":false,"flippedY":false,"uid":"25979726","name":"Torch1"},
 Torch3: {"type":"Anchor","x":488,"y":224,"flippedX":false,"flippedY":false,"uid":"25979726","name":"Torch3"},
-Torch2: {"type":"Anchor","x":384,"y":224,"flippedX":false,"flippedY":false,"uid":"25979726","name":"Torch2"}
+Torch2: {"type":"Anchor","x":384,"y":224,"flippedX":false,"flippedY":false,"uid":"25979726","name":"Torch2"},
+SnowmanSpawn: {"type":"Anchor","x":328,"y":224,"flippedX":false,"flippedY":false,"uid":"25979726","name":"SnowmanSpawn"}
 };
 }
 };
--- src/levels/unrealSnowman.ts
+++ src/levels/unrealSnowman.ts
@@ -6,8 +6,8 @@ import {wait} from "../cutscene/wait";
 import {player} from "../gameObjects/player";
 import {lerp} from "../cutscene/lerp";
 import {subimageTextures} from "../utils/pixi/simpleSpritesheet";
-import {Torch} from "../textures";
-import {Sprite} from "pixi.js";
+import {Snowman, Torch} from "../textures";
+import {Container, Sprite} from "pixi.js";
 import {now} from "../utils/now";
 import {merge} from "../utils/merge";
 import {cutscene} from "../cutscene/cutscene";
@@ -17,8 +17,8 @@ let holdingFlame = false;
 
 export function UnrealSnowman() {
     const level = applyOgmoLevel(UnrealSnowmanArgs);
-    scene.backgroundColor = 0xF0C050;
-    scene.terrainColor = 0xF05050;
+    scene.backgroundColor = 0xC3C2EF;
+    scene.terrainColor = 0xC43E4E;
     portalFluidConfig.gotoLevelName = "DesertTemple";
 
     holdingFlame = false;
@@ -35,6 +35,8 @@ export function UnrealSnowman() {
     level.PuzzleWall.withStep(() => {
         level.PuzzleWall.moveTowards({ x: level.PuzzleWall.x, y: puzzleTorch.burning ? 0 : -level.PuzzleWall.height }, puzzleTorch.burning ? 8 : 2);
     });
+
+    snowman().at(level.SnowmanSpawn);
 }
 
 const torchSubimages = subimageTextures(Torch, 5);
@@ -57,4 +59,33 @@ const torch = (burning = false, showHint = false) => {
         }
     });
     return scene.gameObjectStage.addChild(sprite);
+}
+
+const snowmanSubimages = subimageTextures(Snowman, 3);
+
+const snowman = () => {
+    let pedometer = 0;
+    const sprite = merge(Sprite.from(snowmanSubimages[0]), { hspeed: 0 });
+    const foot1 = Sprite.from(snowmanSubimages[1]);
+    const foot2 = Sprite.from(snowmanSubimages[2]);
+    sprite.withStep(() => {
+       pedometer++;
+       foot1.y = Math.round(Math.sin(pedometer * 0.2));
+        foot2.y = Math.round(Math.sin(pedometer * 0.2 - 3));
+        sprite.y = Math.round(Math.sin(pedometer * 0.2 - 5));
+        if (container.x > player.x) {
+            container.scale.x = -1;
+            container.x -= 1;
+        }
+        else if (container.x < player.x) {
+            container.scale.x = 1;
+            container.x += 1;
+        }
+        if (container.collides(player))
+            player.damage(10);
+    });
+    const container = new Container();
+    container.addChild(foot1, foot2, sprite);
+    container.pivot.set(11, 42);
+    return scene.gameObjectStage.addChild(container);
 }
