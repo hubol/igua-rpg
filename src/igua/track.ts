a766b2e81ddb9240dc7ae329418d31ae9e5b3b1a
Hubol <hubol.gordon@gmail.com>
2021-09-02 18:06:51 -0500

    Improve names
    Former-commit-id: 7bb6be393a1123e52da61e4ba2d48b963bb8184d
    Former-commit-id: 7599fdeb94a6c9a72dcaf4191630e5f3825c1f9e

---
+++ src/igua/track.ts
@@ -0,0 +1,30 @@
+import {merge} from "../utils/merge";
+import {scene} from "./scene";
+import {doNowOrOnAdded} from "../utils/extensions/pixiExtensions";
+
+let nextTrackingId = 0;
+
+function getTrackedDisplayObjects(id) {
+    if (!scene.trackedDisplayObjects[id])
+        scene.trackedDisplayObjects[id] = [];
+    return scene.trackedDisplayObjects[id] as any[];
+}
+
+export const track = <T extends (...args: any) => any>(fn: T) => {
+    const trackingId = nextTrackingId++;
+    const wrappedFn = ((...args) => {
+        const displayObject = fn(...args);
+        displayObject._trackingId = trackingId;
+        doNowOrOnAdded(displayObject, () => {
+            const array = getTrackedDisplayObjects(displayObject._trackingId);
+            array.push(displayObject);
+            displayObject.on('removed', () => array.removeFirst(displayObject));
+        })
+        return displayObject;
+    }) as T;
+    return merge(wrappedFn, {
+        get instances() {
+            return getTrackedDisplayObjects(trackingId) as ReturnType<typeof fn>[];
+        }
+    });
+}
