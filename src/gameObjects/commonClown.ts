95934bf091b5a372ecff712c2d57d76357fdeb30
Hubol <hubol.gordon@gmail.com>
2021-09-23 23:17:44 -0500

    Tweak common clown animation
    Former-commit-id: 81267e21f9029a90dea60ac5dceccd69fdcc21ad
    Former-commit-id: 5ce8465fea63239fe9a196e2e02cd04a41d863da

--- src/gameObjects/commonClown.ts
+++ src/gameObjects/commonClown.ts
@@ -17,9 +17,9 @@ export function commonClown() {
         graphics.clear();
         graphics.lineStyle(1, 0x888888);
         for (let i = 0; i < 1; i += 0.05)
-            graphics.lineTo(Math.sin(i * Math.PI * 2.25 * 2 + radians) * 6 * Math.min(1, i * 2) * invertedScale, i * 10 * scale);
-        offsetContainer.y = 120 * (1 - unit);
-        if (Math.random() > 0.6)
+            graphics.lineTo(Math.sin(i * Math.PI * 2.25 * 2 + radians) * 6 * Math.min(1, i * 2) * invertedScale - Math.pow(i, 2) * scale, i * 10 * scale);
+        offsetContainer.y = 120 * (1 - unit) - 120;
+        if (offsetContainer.y < -20 && Math.random() > 0.1)
         container.x++;
     })
     offsetContainer.addChild(graphics, sprite);
