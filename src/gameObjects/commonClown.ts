08d87e26592be63ba9ba48356e12d32ef7333ec2
Hubol <hubol.gordon@gmail.com>
2021-09-24 22:42:50 -0500

    Common clown is closer to being an enemy
    Former-commit-id: 1b0157af0e7032edd473ec419d0a20a76b218935
    Former-commit-id: d2f74874b2a28b24d526f3a501e9872001cc869d

--- src/gameObjects/commonClown.ts
+++ src/gameObjects/commonClown.ts
@@ -1,16 +1,22 @@
 import {Container, Graphics, Sprite} from "pixi.js";
 import {CommonClown} from "../textures";
 import {scene} from "../igua/scene";
-import {now} from "../utils/now";
+import {merge} from "../utils/merge";
+import { lerp } from "../utils/math/number";
+import {isOnGround} from "./walls";
 
 export function commonClown() {
-    const container = new Container();
+    const container = merge(new Container(), { hspeed: 0.75, vspeed: 0 });
     const sprite = Sprite.from(CommonClown);
     const graphics = new Graphics();
-    const offsetContainer = new Container();
     sprite.anchor.set(.5, 1);
+
+    let unit = 0;
+
     sprite.withStep(() => {
-        const unit = (Math.sin(now.s * 5) + 1) / 2;
+        unit = lerp(unit, container.vspeed < 0 ? 1 : 0, 0.0875);
+        container.vspeed += 0.25;
+
         const radians = Math.PI * unit * 2;
         const scale = 0.5 + unit * 2;
         const invertedScale = Math.max(0.95 - unit, 0);
@@ -18,11 +24,16 @@ export function commonClown() {
         graphics.lineStyle(1, 0x888888);
         for (let i = 0; i < 1; i += 0.05)
             graphics.lineTo(Math.sin(i * Math.PI * 2.25 * 2 + radians) * 6 * Math.min(1, i * 2) * invertedScale - Math.pow(i, 2) * scale, i * 10 * scale);
-        offsetContainer.y = 120 * (1 - unit) - 120;
-        if (offsetContainer.y < -20 && Math.random() > 0.1)
-        container.x++;
+        if (container.vspeed > -5 && container.vspeed < 5)
+            container.x += container.hspeed;
+
+        const spring = 10 * scale;
+        const radius = Math.max(8, container.vspeed);
+        const pushable = { x: container.x, y: container.y + spring - radius };
+        if (container.vspeed > 0 && isOnGround(pushable, radius))
+            container.vspeed = -6;
+        container.y += container.vspeed;
     })
-    offsetContainer.addChild(graphics, sprite);
-    container.addChild(offsetContainer);
+    container.addChild(graphics, sprite);
     return scene.gameObjectStage.addChild(container);
 }
