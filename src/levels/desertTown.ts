bde0192bd1dc4cf9b0acc598ef6de9a8504bd1fc
Hubol <hubol.gordon@gmail.com>
2021-12-20 23:15:46 -0600

    Warm the shop music while in the desert town

--- src/levels/desertTown.ts
+++ src/levels/desertTown.ts
@@ -1,7 +1,7 @@
 import {applyOgmoLevel} from "../igua/level/applyOgmoLevel";
 import {DesertTownArgs} from "../levelArgs";
 import {jukebox} from "../igua/jukebox";
-import {Bluehouse, Country, Oracle, DesertTown as DesertField} from "../musics";
+import {Bluehouse, Country, Oracle, DesertTown as DesertField, Shop} from "../musics";
 import {Sprite} from "pixi.js";
 import {CrateWooden} from "../textures";
 import {isOnScreen} from "../igua/logic/isOnScreen";
@@ -30,7 +30,7 @@ type DesertTownLevel = ReturnType<typeof getDesertTownLevel>;
 
 export function DesertTown()
 {
-    jukebox.play(Country).warm(Oracle, Bluehouse, DesertField);
+    jukebox.play(Country).warm(Oracle, Bluehouse, Shop, DesertField);
     const level = getDesertTownLevel();
     scene.backgroundColor = 0xF0F0B0;
     scene.terrainColor = 0xE0D060;
