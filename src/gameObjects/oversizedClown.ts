cbf96913190926cc5ebb1044f2426a160c637c3b
Hubol <hubol.gordon@gmail.com>
2022-01-12 22:01:21 -0600

    pretty cute oversized angel behavior!

--- src/gameObjects/oversizedClown.ts
+++ src/gameObjects/oversizedClown.ts
@@ -7,16 +7,18 @@ import {player} from "./player";
 import {approachLinear} from "../utils/math/number";
 import {scene} from "../igua/scene";
 import {bouncePlayer} from "../igua/bouncePlayer";
-import {empBlast} from "./empBlast";
-import {rng} from "../utils/rng";
 import {range} from "../utils/range";
 import {ballons} from "./ballons";
+import {push} from "./walls";
+import {sleep} from "../cutscene/sleep";
+import {empBlast} from "./empBlast";
+import {wait} from "../cutscene/wait";
 
 const [headTexture, faceTexture, hairTexture, leftBrowTexture, rightBrowTexture] =
     subimageTextures(OversizedAngel, { width: 66 });
 
 export function oversizedClown() {
-    const faceState = { anger: 0 };
+    const faceState = { anger: 0, excited: 0 };
 
     const fullHealth = 30;
     let health = fullHealth;
@@ -32,7 +34,55 @@ export function oversizedClown() {
 
     let initialY: number;
 
-    c.withStep(() => {
+    function nothing() { }
+
+    function fall() {
+        speed.y += 0.3;
+        c.add(speed);
+        const pushable = [33, 25].add(c);
+        const r = push(pushable, 26);
+        c.add(pushable.add(c, -1).add(-33, -25));
+        if (r.isOnGround)
+            c.withAsync(startLanded);
+    }
+
+    function landed() {
+
+    }
+
+    async function doEmpBlast() {
+        const unit = health / fullHealth;
+        let hints = 3;
+        if (unit < .33)
+            hints = 1;
+        else if (unit < .67)
+            hints = 2;
+        const emp = empBlast(128, hints, 34, 1000).at([33, 25].add(c)).show();
+        await wait(() => emp.destroyed);
+    }
+
+    async function startLanded() {
+        setBehavior(landed);
+        await doEmpBlast();
+        await sleep(250);
+        restartHostile();
+    }
+
+    function restartHostile() {
+        for (let i = 0; i < 5; i++)
+            ballonsState.push(1);
+        setBehavior(hostile);
+    }
+
+    async function startFalling() {
+        faceState.excited = 1;
+        setBehavior(nothing);
+        await sleep(1000);
+        faceState.excited = 0;
+        setBehavior(fall);
+    }
+
+    function hostile() {
         if (initialY === undefined)
             initialY = c.y;
         if (health < fullHealth * 0.33)
@@ -48,8 +98,13 @@ export function oversizedClown() {
             speed.x -= player.engine.knockback.x;
             speed.y -= player.vspeed * 0.3;
             ballonsState[ballonIndex] = 0;
-            if (!hasBallons)
-                empBlast(128, rng.int(3) + 1, 50, 1000).at([33, 25].add(c)).show();
+
+            const justLostAllBallons = ballonsState.findIndex(x => x === 1) === -1;
+
+            if (justLostAllBallons) {
+                c.withAsync(startFalling);
+                return;
+            }
         }
 
         if (c.y > initialY && hasBallons)
@@ -60,6 +115,16 @@ export function oversizedClown() {
             speed.y += Math.sin(now.s) * 0.02;
         c.add(speed);
         speed.scale(0.875);
+    }
+
+    function setBehavior(fn: () => void) {
+        speed.at(0, 0);
+        behavior = fn;
+    }
+    let behavior = hostile;
+
+    c.withStep(() => {
+        behavior();
     });
 
     ballons({ target: c, state: ballonsState, string: 32, offset: [33, 11] });
@@ -67,7 +132,7 @@ export function oversizedClown() {
     return c;
 }
 
-function face(state: { anger: number }) {
+function face(state: { anger: number, excited: number }) {
     const f = Sprite.from(faceTexture);
     const ll = Sprite.from(leftBrowTexture);
     ll.anchor.set(32/66, 14/51);
@@ -78,18 +143,18 @@ function face(state: { anger: number }) {
 
     l.withStep(() => {
         l.pivot.x = -32;
-        l.pivot.y = -14 + Math.sin(now.s * Math.PI * 2.3 + 2);
+        l.pivot.y = -14 + Math.sin(now.s * Math.PI * 2.3 * (1 + state.excited) + 2) * (1 + state.excited);
         ll.angle = state.anger * 30 + Math.cos(now.s * Math.PI * 3.3 - 2) * 10 * state.anger;
     });
     r.withStep(() => {
         r.pivot.x = -49;
-        r.pivot.y = -14 + Math.sin(now.s * Math.PI * 2.1 + 5);
+        r.pivot.y = -14 + Math.sin(now.s * Math.PI * 2.1 * (1 + state.excited) + 5) * (1 + state.excited);
         rr.angle = state.anger * -30 + Math.cos(now.s * Math.PI * 2.9 - 4) * 10 * state.anger;
     });
 
     const c = container(f, l, r)
     c.withStep(() => {
-        c.pivot.y = Math.sin(now.s * Math.PI * 2);
+        c.pivot.y = Math.sin(now.s * Math.PI * 2 * (1 + state.excited)) * (1 + state.excited);
     });
     c.pivot.x = 41;
     c.scale.x = 1;
