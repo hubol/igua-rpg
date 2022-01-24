49a0ff80c6d8cf623417e3bc2ac6207889f4f300
Hubol <hubol.gordon@gmail.com>
2022-01-24 02:22:29 -0600

    The mirror looks cuter now

--- src/gameObjects/mirror.ts
+++ src/gameObjects/mirror.ts
@@ -10,10 +10,17 @@ export function mirror(width, height) {
         m.ty = -c.y;
         game.renderer.render(player, texture, true, m);
     });
-    const g = new Graphics().beginFill(0xC0D6E5).drawRect(0, 0, width, height);
+    function rect() {
+        return new Graphics().beginFill(0xC0D6E5).drawRect(0, 0, width, height);
+    }
+    const g = rect()
+        .lineStyle(4, 0xD5E6F2).moveTo(16, -3).lineTo(-3, 16)
+        .lineStyle(2, 0xD5E6F2).moveTo(32, -3).lineTo(-3, 32)
+        .lineStyle(1, 0xD5E6F2).moveTo(width + 3, height - 16).lineTo(width + 3 - 16, height);
+    g.mask = rect();
     const texture = RenderTexture.create({ width, height });
     const s = Sprite.from(texture);
     s.alpha = 0.5;
-    c.addChild(g, s);
+    c.addChild(g.mask, g, s);
     return c;
 }
