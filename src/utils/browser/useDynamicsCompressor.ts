64eaae5bbd783f914dfd60db94cd2757a987c25f
Hubol <hubol.gordon@gmail.com>
2022-02-01 23:06:03 -0600

    Suppress inexplicable exception when constructing DynamicsCompressorNode on iOS

--- src/utils/browser/useDynamicsCompressor.ts
+++ src/utils/browser/useDynamicsCompressor.ts
@@ -1,10 +1,19 @@
 import {Howler} from "howler";
 
 export function useDynamicsCompressor() {
-    if (!Howler.masterGain || !DynamicsCompressorNode)
+    if (!Howler.masterGain)
         return;
+    let compressor: DynamicsCompressorNode;
+
+    try {
+        compressor = new DynamicsCompressorNode(Howler.ctx);
+    }
+    catch (e) {
+        console.warn('Failed to create DynamicsCompressorNode', e);
+        return;
+    }
+
     const destination = Howler.ctx.destination;
-    const compressor = new DynamicsCompressorNode(Howler.ctx);
     compressor.ratio.value = 6;
     compressor.connect(destination);
     Howler.masterGain.disconnect(destination);
