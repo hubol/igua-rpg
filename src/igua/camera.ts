67842f51b79dbda55d074b3a52a3ad879f496b69
Hubol <hubol.gordon@gmail.com>
2022-02-14 18:18:22 -0600

    Revert rounding for player and camera as it seems to have adverse effects on custom characters(?)

--- src/igua/camera.ts
+++ src/igua/camera.ts
@@ -2,23 +2,19 @@ import {scene} from "./scene";
 
 export function camera(followPlayer: boolean)
 {
-    let _x = 0;
-    let _y = 0;
     return {
         get x() {
-            return _x;
+            return -scene.cameraStage.x;
         },
         get y() {
-            return _y;
+            return -scene.cameraStage.y;
         },
         set x(value) {
-            _x = value;
-            scene.cameraStage.x = -Math.round(value);
+            scene.cameraStage.x = -value;
             scene.parallax1Stage.x = Math.round(-value * 0.9);
         },
         set y(value) {
-            _y = value;
-            scene.cameraStage.y = -Math.round(value);
+            scene.cameraStage.y = -value;
             scene.parallax1Stage.y = Math.round(-value * 0.9);
         },
         width: 256,
