3a61b504f0fbf568ebf0212fc2f16eb900621070
Hubol <hubol.gordon@gmail.com>
2022-04-20 02:25:35 -0500

    move camera to player target did strange stuff in unreal royal chamber

--- src/igua/camera.ts
+++ src/igua/camera.ts
@@ -3,6 +3,8 @@ import {Container} from "pixi.js";
 import {computePlayerCameraTarget} from "./playerCamera";
 import {moveTowards} from "../utils/math/vector";
 import {wait} from "../cutscene/wait";
+import {trackPosition} from "./trackPosition";
+import {player} from "../gameObjects/player";
 
 export function camera(followPlayer: boolean)
 {
@@ -31,12 +33,18 @@ export function moveCameraToPlayerTarget(speed: number) {
     const tp = computePlayerCameraTarget().vcpy();
     const c = new Container().withStep(() => {
         const t = computePlayerCameraTarget();
-        scene.camera.add(t.vcpy().add(tp, -1));
+
+        if (playerPosition.diff.x !== 0 || playerPosition.diff.y !== 0) {
+            scene.camera.add(t.vcpy().add(tp, -1));
+            tp.at(t);
+        }
+
         moveTowards(scene.camera, t, speed);
-        tp.at(t);
         if (scene.camera.x === t.x && scene.camera.y === t.y)
             c.destroy();
     }).show();
+
+    const playerPosition = trackPosition(player);
 
     return wait(() => c.destroyed);
 }
