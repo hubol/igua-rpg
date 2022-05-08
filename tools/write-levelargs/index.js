224f253c1734bee29ca54d3b8436a5a7449e93de
Hubol <hubol.gordon@gmail.com>
2022-05-07 19:43:57 -0500

    Update source for write-levelargs

--- tools/write-levelargs/index.js
+++ tools/write-levelargs/index.js
@@ -1,2 +1,3 @@
+// https://dev.to/michaeljota/electron-with-typescript-using-ts-node-8oi
 require('ts-node').register(); // This will register the TypeScript compiler
 require('./src'); // This will load our Typescript application
