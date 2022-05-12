e78ba1fd6cebadd20ba296a6574093d68863d36b
Hubol <hubol.gordon@gmail.com>
2022-05-11 20:52:03 -0500

    Adjust moveCameraToPlayerTarget. Who knows what the consequences might be

--- src/igua/camera.ts
+++ src/igua/camera.ts
@@ -3,8 +3,6 @@ import {Container} from "pixi.js";
 import {computePlayerCameraTarget} from "./playerCamera";
 import {moveTowards} from "../utils/math/vector";
 import {wait} from "../cutscene/wait";
-import {trackPosition} from "./trackPosition";
-import {player} from "../gameObjects/player";
 
 export function camera(followPlayer: boolean)
 {
@@ -30,21 +28,13 @@ export function camera(followPlayer: boolean)
 }
 
 export function moveCameraToPlayerTarget(speed: number) {
-    const tp = computePlayerCameraTarget().vcpy();
     const c = new Container().withStep(() => {
         const t = computePlayerCameraTarget();
-
-        if (playerPosition.diff.x !== 0 || playerPosition.diff.y !== 0) {
-            scene.camera.add(t.vcpy().add(tp, -1));
-            tp.at(t);
-        }
 
         moveTowards(scene.camera, t, speed);
         if (scene.camera.x === t.x && scene.camera.y === t.y)
             c.destroy();
     }).show();
-
-    const playerPosition = trackPosition(player);
 
     return wait(() => c.destroyed);
 }
