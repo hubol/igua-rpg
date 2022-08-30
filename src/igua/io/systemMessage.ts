aa34f6fd75b778dc91a266855c1cbac67ab8eeed
Hubol <hubol.gordon@gmail.com>
2022-08-30 00:49:15 -0500

    system message does not overlap

--- src/igua/io/systemMessage.ts
+++ src/igua/io/systemMessage.ts
@@ -1,15 +1,41 @@
 import {IguaText} from "../text";
 import {game} from "../game";
-import {sleep} from "../../cutscene/sleep";
-import {lerp} from "../../cutscene/lerp";
+import {Undefined} from "../../utils/types/undefined";
+import {merge} from "../../utils/object/merge";
+
+let textInstance = Undefined<ReturnType<typeof newText>>();
+
+function newText() {
+    let phase = 0;
+
+    function setText(text: string, tint = 0xffffff) {
+        phase = 0;
+        t.text = text;
+        t.tint = tint;
+    }
+
+    const t = merge(IguaText.Large(''), { setText } )
+        .at(2, 242)
+        .withStep(() => {
+            if (phase < 0)
+                return;
+            phase += 1;
+            if (phase < 60) {
+                t.alpha = 1;
+            }
+            else if (phase < 60 * 3) {
+                t.alpha -= 1 / 120;
+            }
+            else
+                phase = -1;
+        })
+        .show(game.hudStage);
+
+    return t;
+}
 
 export function systemMessage(text: string, tint = 0xffffff) {
-    const t = IguaText.Large(text, { tint })
-        .at(2, 242)
-        .show(game.hudStage)
-        .withAsync(async () => {
-            await sleep(1_000);
-            await lerp(t, 'alpha').to(0).over(2_000);
-            t.destroy();
-        });
+    if (!textInstance)
+        textInstance = newText();
+    textInstance.setText(text, tint);
 }
