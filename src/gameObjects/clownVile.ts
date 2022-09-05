189421845db17fa4677b156eee44736dfd53d318
Hubol <hubol.gordon@gmail.com>
2022-09-05 12:47:03 -0500

    More expressions for vile clown

--- src/gameObjects/clownVile.ts
+++ src/gameObjects/clownVile.ts
@@ -24,15 +24,20 @@ import {Force} from "../utils/types/force";
 
 enum Expression {
     Default,
-    Happy
+    Happy,
+    Surprise,
+    Angry,
+    Evil,
+    ChargeSpit,
+    Spit
 }
 
 export function clownVile() {
     const controls = {
         eyeL: { pos: vnew(), img: 0 },
         eyeR: { pos: vnew(), img: 0 },
-        eyebrowsF: 0,
-        mouth: { img: 0 },
+        eyebrows: { img: 0, y: 0 },
+        mouth: { pos: vnew(), img: 0 },
         facing: vnew(),
         blink: 0,
         unblink: 0,
@@ -42,36 +47,98 @@ export function clownVile() {
         facePlayer: true,
         lookAtPlayer: true,
         widenEyes: false,
+        wiggleEyebrows: 0,
     }
 
     const defaultAutomation = { ...automation };
 
+    function resetAutomation() {
+        Object.assign(automation, defaultAutomation);
+    }
+    function resetControls() {
+        controls.mouth.pos.at(0, 0);
+        controls.mouth.img = 0;
+        controls.eyeL.img = 0;
+        controls.eyeR.img = 0;
+        controls.eyebrows.img = 0;
+        controls.eyebrows.y = 0;
+    }
+
     let expression = Expression.Default;
 
     function showExpression() {
-        if (expression === Expression.Default) {
-            Object.assign(automation, defaultAutomation);
-            controls.mouth.img = 0;
-            controls.eyeL.img = 0;
-            controls.eyeR.img = 0;
-            controls.eyebrowsF = 0;
-        }
-        else if (expression === Expression.Happy) {
-            controls.eyeL.pos.scale(0);
-            controls.eyeR.pos.scale(0);
+        if (expression === Expression.Happy) {
+            moveTowards(controls.eyeL.pos, [0, 0], 1);
+            moveTowards(controls.eyeR.pos, [0, 0], 1);
+            controls.eyeL.pos.y += Math.sin(now.s * Math.PI * 5) * 0.67;
+            controls.eyeR.pos.y += Math.sin(now.s * Math.PI * 5) * 0.67
             automation.facePlayer = true;
             automation.lookAtPlayer = false;
             automation.widenEyes = true;
+            automation.wiggleEyebrows = 0;
             controls.mouth.img = 1;
             controls.eyeL.img = 1;
             controls.eyeR.img = 1;
             controls.facing.y = Math.min(0, controls.facing.y);
-            controls.eyebrowsF = 1;
+        }
+        else if (expression === Expression.Surprise) {
+            automation.facePlayer = false;
+            automation.lookAtPlayer = true;
+            automation.widenEyes = true;
+            automation.wiggleEyebrows = 1;
+            controls.mouth.img = 2;
+        }
+        else if (expression === Expression.Angry) {
+            automation.facePlayer = false;
+            automation.lookAtPlayer = true;
+            controls.mouth.img = 3;
+            controls.eyebrows.img = 1;
+        }
+        else if (expression === Expression.Evil) {
+            const f = now.s * Math.PI * 3;
+            const v = vnew().at(Math.sin(f) * 3, Math.cos(f) * 6);
+            const s = 2;
+            moveTowards(controls.eyeL.pos, v, s);
+            moveTowards(controls.eyeR.pos, v, s);
+            automation.widenEyes = true;
+            controls.mouth.img = 1;
+            controls.eyebrows.img = 1;
+        }
+        else if (expression === Expression.ChargeSpit) {
+            automation.widenEyes = true;
+            controls.mouth.img = Math.min(7, controls.mouth.img + 0.1);
+            controls.eyebrows.y = Math.max(-2, controls.eyebrows.y - 0.1);
+        }
+        else if (expression === Expression.Spit) {
+            controls.eyeL.pos.vlength = Math.min(controls.eyeL.pos.vlength, 3);
+            controls.eyeR.pos.vlength = Math.min(controls.eyeR.pos.vlength, 3);
+            automation.lookAtPlayer = false;
+            moveTowards(controls.mouth.pos, controls.facing.vcpy().scale(2), 0.2);
+            automation.widenEyes = true;
+            automation.facePlayer = false;
+            controls.mouth.img = 8;
+            controls.eyebrows.img = 1;
+            controls.eyeL.img = 2;
+            controls.eyeR.img = 3;
         }
     }
 
+    function setExpression(e: Expression) {
+        resetAutomation();
+        resetControls();
+
+        if (e === Expression.ChargeSpit) {
+            controls.mouth.img = 4;
+            controls.eyebrows.y = 2;
+        }
+
+        expression = e;
+
+        showExpression();
+    }
+
     function newEye(control: { img: number }) {
-        const eyebrow = Sprite.from(VileClownEyebrow);
+        const eyebrow = Sprite.from(eyebrowTxs[0]);
         const mask = Sprite.from(eyelidTxs.last);
         const eyelid = Sprite.from(eyelidTxs[0]);
         const pupil = Sprite.from(pupilTxs[0]).filter(alphaMaskFilter(mask));
@@ -82,7 +149,8 @@ export function clownVile() {
                 const closed = nlerp(nlerp(min, 1, c.blink), 0, controls.unblink);
                 pupil.texture = pupilTxs[control.img];
                 eyelid.texture = eyelidTxs[Math.floor(Math.max(0, Math.min(1, closed) * eyelidTxs.length))];
-                eyebrow.y = Math.sin(now.s * Math.PI * 6) * controls.eyebrowsF;
+                eyebrow.texture = eyebrowTxs[controls.eyebrows.img];
+                eyebrow.y = Math.sin(now.s * Math.PI * 6) * automation.wiggleEyebrows + controls.eyebrows.y;
             });
 
         eyebrow.pivot.at(1, 7);
@@ -95,7 +163,7 @@ export function clownVile() {
     let __center = Force<DisplayObject>();
 
     function newHead() {
-        const mask = Sprite.from(VileClownHead);
+        const mask = Sprite.from(VileClownHead).at(0, 1);
         const sprite = Sprite.from(VileClownHead);
         __center = sprite;
         const eyeL = newEye(controls.eyeL);
@@ -131,7 +199,8 @@ export function clownVile() {
         }
 
         function updateMouth() {
-            mouth.texture = mouthTxs[controls.mouth.img];
+            mouth.texture = mouthTxs[Math.floor(controls.mouth.img)];
+            mouth.pivot.at(controls.mouth.pos).scale(-1);
         }
 
         face
@@ -204,10 +273,10 @@ export function clownVile() {
         })
         .withAsync(async () => {
             while (true) {
-                expression = Expression.Default;
-                await sleep(2000);
-                expression = Expression.Happy;
-                await sleep(2000);
+                for (let i = 0; i < Object.keys(Expression).length / 2; i++) {
+                    setExpression(i);
+                    await sleep(2000);
+                }
             }
         });
 
@@ -216,5 +285,6 @@ export function clownVile() {
 
 const eyelidTxs = subimageTextures(VileClownEyelid, 12);
 const mouthTxs = subimageTextures(VileClownMouth, { width: 32 });
+const eyebrowTxs = subimageTextures(VileClownEyebrow, { width: 16 });
 const pupilTxs = subimageTextures(VileClownPupil, { width: 10 });
 const hairTxs = subimageTextures(VileClownHair, 4);
