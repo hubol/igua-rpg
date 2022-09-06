2d6ca416c40da66126d0e61e1a892d88054454bc
Hubol <hubol.gordon@gmail.com>
2022-09-06 03:17:47 -0500

    Improvements to healthbar animations

--- src/igua/ui/healthbar.ts
+++ src/igua/ui/healthbar.ts
@@ -4,7 +4,7 @@ import {lerp} from "../../utils/math/number";
 const states = new WeakMap<object, ReturnType<typeof newState>>();
 
 function newState(value: number) {
-    return { target: value, value, s: now.s };
+    return { target: value, from: value, s: now.s };
 }
 
 const result = { life: 0, hurt: 0, heal: 0 };
@@ -15,26 +15,29 @@ export function healthbar(identity: object, value: number, max: number) {
         states.set(identity, state = newState(max));
 
     if (value !== state.target) {
-        if (Math.abs(value - state.target) > 0.01 * max)
+        if (Math.abs(value - state.target) > 0.01 * max) {
             state.s = now.s;
+            state.from = state.target;
+        }
         state.target = value;
     }
 
     const since = now.s - state.s;
-    const f = Math.min(1, since * 2);
-    state.value = lerp(state.value, state.target, f);
+    const f = Math.min(1, since * 4);
+    const y = 1 - Math.pow(1 - f, 3);
+    const lvalue = lerp(state.from, state.target, y);
 
     result.life = 0;
     result.hurt = 0;
     result.heal = 0;
 
-    if (state.value < state.target) {
-        result.life = state.value;
+    if (lvalue < state.target) {
+        result.life = lvalue;
         result.heal = state.target;
     }
-    else if (state.value > state.target) {
+    else if (lvalue > state.target) {
         result.life = state.target;
-        result.hurt = state.value;
+        result.hurt = lvalue;
     }
     else {
         result.life = value;
