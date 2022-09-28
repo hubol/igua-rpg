39a6c4840f29b1e631c9505debf4af391d6cb23b
Hubol <hubol.gordon@gmail.com>
2022-09-28 14:19:55 -0500

    Pixin can now easily restrict its minimum type

--- src/utils/pixi/pixin.ts
+++ src/utils/pixi/pixin.ts
@@ -5,11 +5,24 @@ import {merge} from "../object/merge";
 export function Pixin<TValues extends {} = {}>(defaults = {} as TValues) {
     return {
         applies<TSrc extends DisplayObject, TDst extends TSrc>(apply: (src: TSrc & TValues) => TDst) {
-            return function(overrides = {} as Partial<TValues>) {
-                const args = applyDefaults(defaults, overrides);
-                return <PixinType<TSrc, Omit<TDst, keyof TSrc>>>((src: TSrc) => apply(merge(src, args)));
+            return applies<TSrc, TValues, TDst>(defaults, apply);
+        },
+        restricted<TSrc extends DisplayObject>() {
+            return {
+                applies<TDst extends TSrc>(apply: (src: TSrc & TValues) => TDst) {
+                    return applies<TSrc, TValues, TDst>(defaults, apply);
+                }
             }
         }
+    }
+}
+
+function applies<TSrc extends DisplayObject, TValues extends {}, TDst extends TSrc>(defaults: TValues, apply: (src: TSrc & TValues) => TDst) {
+    type Features = Omit<TDst, keyof TSrc>;
+
+    return function(overrides = {} as Partial<TValues>) {
+        const args = applyDefaults(defaults, overrides);
+        return <PixinType<TSrc, Features>>((src: TSrc) => apply(merge(src, args)));
     }
 }
 
