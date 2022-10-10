b95d9fe0003961a7d0d7decce12bd0742121f1f6
Hubol <hubol.gordon@gmail.com>
2022-10-10 00:17:36 -0500

    Capital bubbles are now animated. Fixed capital bubbles being invisible even with sufficient intelligence

--- src/gameObjects/capitalBubble.ts
+++ src/gameObjects/capitalBubble.ts
@@ -5,12 +5,25 @@ import {progress} from "../igua/data/progress";
 import {player} from "./player";
 import {nlerp} from "../utils/math/number";
 import {resolveGameObject} from "../igua/level/resolveGameObject";
+import {rng} from "../utils/math/rng";
+import {now} from "../utils/now";
 
 export const resolveCapitalBubble = resolveGameObject('CapitalBubble', a => capitalBubble(a.obscurity).at(a));
 
 function capitalBubble(obscurity: number) {
+    const x = rng() * 4;
+    const dx = (0.2 + rng() * 0.3) * 4;
+
     const s = Sprite.from(CapitalBubble)
-        .withAsync(async () => resolveBlock({ x: s.x, y: s.y + 10, width: 28, height: 7, visible: false } as any));
+        .centerAnchor()
+        .withStep(() => {
+            s.scale.x = 0.8 + Math.round(Math.sin(now.s * dx + x)) * 0.05;
+            s.scale.y = 0.8 + Math.round(Math.cos(now.s * dx * 0.87 - x * 3)) * 0.1;
+        })
+        .withAsync(async () => {
+            s.add(14, 14);
+            resolveBlock({x: s.x - 14, y: s.y + 10 - 14, width: 28, height: 7, visible: false} as any);
+        });
 
     if (obscurity > 0)
         s.withStep(() => {
@@ -23,7 +36,7 @@ function capitalBubble(obscurity: number) {
 }
 
 function alphaBase(obscurity: number) {
-    return obscurity > progress.levels.intelligence ? 0 : ((obscurity + 1) - progress.levels.intelligence) / 10;
+    return obscurity > progress.levels.intelligence ? 0 : ((progress.levels.intelligence + 1) - obscurity) / 10;
 }
 
 function alphaMax(obscurity: number) {
