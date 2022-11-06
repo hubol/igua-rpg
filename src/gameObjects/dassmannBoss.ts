a5ca8e93aeafd6ec8a878e01a907a4d8e7a5956c
Hubol <hubol.gordon@gmail.com>
2022-11-06 11:00:11 -0600

    Dassmann boss can now flee after tower is broken

--- src/gameObjects/dassmannBoss.ts
+++ src/gameObjects/dassmannBoss.ts
@@ -10,9 +10,11 @@ import {Undefined} from "../utils/types/undefined";
 import {DisplayObject} from "pixi.js";
 import {waitHold} from "../cutscene/waitHold";
 import {hDistFromPlayer} from "../igua/logic/getOffsetFromPlayer";
-import {ClownHurt, DassBuildTower} from "../sounds";
+import {ClownHurt, DassBuildTower, SpiderUp} from "../sounds";
 import {Invulnerable} from "../pixins/invulnerable";
-import {bouncePlayer, bouncePlayerOffDisplayObject} from "../igua/bouncePlayer";
+import {bouncePlayerOffDisplayObject} from "../igua/bouncePlayer";
+import {FreeSpace} from "../pixins/freeSpace";
+import {approachLinear} from "../utils/math/number";
 
 const consts = {
     damage: {
@@ -23,7 +25,8 @@ const consts = {
 
 export function dassmannBoss() {
     const d = dassmann()
-        .withPixin(Invulnerable());
+        .withPixin(Invulnerable())
+        .withPixin(FreeSpace({ offsets: [[0, -8]] }));
     d.friction = consts.friction;
     const { body, head, arml, armr } = d;
 
@@ -64,6 +67,8 @@ export function dassmannBoss() {
             self.on('removed', () => !poker.destroyed && poker.destroy());
 
             await health.tookDamage();
+            arml.pose = ArmTx.Rest;
+            armr.pose = ArmTx.Rest;
         })
         .withAsync(async self => {
             while (true) {
@@ -73,11 +78,58 @@ export function dassmannBoss() {
             }
         });
 
+    const flee = attack({ hspeed: 0, canJump: false })
+        .withAsyncOnce(async (self) => {
+            self.hspeed = 3.3 * (d.freeSpaceOnLeft() > d.freeSpaceOnRight() ? -1 : 1);
+            d.friction = 0;
+            const fn = self.hspeed < 0 ? d.freeSpaceOnLeft : d.freeSpaceOnRight;
+            await Promise.all([
+                wait(() => fn() < 64 || hDistFromPlayer(d) > 128).then(() => self.hspeed = 0),
+                sleep(250)
+            ]);
+        })
+        .withAsync(async () => {
+            arml.pose = ArmTx.Rest;
+            while (true) {
+                await arml.raise().over(250);
+                await sleep(125);
+                await arml.rest().over(250);
+                await sleep(125);
+            }
+        })
+        .withAsync(async () => {
+            armr.pose = ArmTx.Raise;
+            while (true) {
+                await armr.rest().over(250);
+                await sleep(125);
+                await armr.raise().over(250);
+                await sleep(125);
+            }
+        })
+        .withAsync(async self => {
+            while (true) {
+                await waitHold(() => !self.canJump, 30);
+                self.canJump = true;
+            }
+        })
+        .withStep(self => {
+            d.speed.x = approachLinear(d.speed.x, self.hspeed, 0.2);
+            if (self.canJump && d.isOnGround && hDistFromPlayer(d) < 48) {
+                SpiderUp.play();
+                self.canJump = false;
+                d.speed.y = -4.3;
+            }
+        })
+        .withCleanup(() => {
+            d.friction = consts.friction;
+        });
+
     async function doAs() {
         await sleep(1000);
         while (true) {
             await attacks.run(buildTower());
-            await sleep(2000);
+            await sleep(1000);
+            await attacks.run(flee());
         }
     }
 
@@ -98,6 +150,7 @@ export function dassmannBoss() {
             d.invulnerable = 15;
             bouncePlayerOffDisplayObject(d);
             d.speed.x -= player.engine.knockback.x;
+            d.speed.y = player.vspeed * -1;
             ClownHurt.play();
             if (health.damage()) {
                 onDeath();
---
+++ src/pixins/freeSpace.ts
@@ -0,0 +1,33 @@
+import {Pixin} from "../utils/pixi/pixin";
+import {Vector, vnew} from "../utils/math/vector";
+import {rayIntersectsWallDistance} from "../igua/logic/rayIntersectsWall";
+import {merge} from "../utils/object/merge";
+
+const unitv = {
+    left: vnew().at(-1, 0),
+    right: vnew().at(1, 0),
+}
+
+function freeSpaceOn(src: Vector, offsets: Vector[], unit: Vector) {
+    return Math.min(
+        ...offsets.map(offset => rayIntersectsWallDistance(v.at(src).add(offset), unit)));
+}
+
+const v = vnew();
+
+export const FreeSpace = Pixin({ offsets: <Vector[]>[] })
+    .applies((src) => {
+        function freeSpaceOnLeft() {
+            return freeSpaceOn(src, src.offsets, unitv.left);
+        }
+
+        function freeSpaceOnRight() {
+            return freeSpaceOn(src, src.offsets, unitv.right);
+        }
+
+        function freeSpaceMin() {
+            return Math.min(freeSpaceOnLeft(), freeSpaceOnRight());
+        }
+
+        return merge(src, { freeSpaceOnLeft, freeSpaceOnRight, freeSpaceMin });
+    });
