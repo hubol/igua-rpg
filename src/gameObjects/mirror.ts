6faee05ecc58c2615d1c53916c0894296d19c7f4
Hubol <hubol.gordon@gmail.com>
2023-01-09 04:04:36 -0600

    Add mirror door to the oversized door

--- src/gameObjects/mirror.ts
+++ src/gameObjects/mirror.ts
@@ -2,19 +2,15 @@ import {Graphics, Matrix, RenderTexture, Sprite} from "pixi.js";
 import {container} from "../utils/pixi/container";
 import {player} from "./player";
 import {game} from "../igua/game";
-import {Undefined} from "../utils/types/undefined";
+import {SceneLocal} from "../igua/sceneLocal";
+import {scene} from "../igua/scene";
+
+const matrix = new Matrix();
+const playerTexture = RenderTexture.create({ width: 256, height: 256 });
 
 export function mirror(width, height, color = 0xC0D6E5, lineColor = 0xD5E6F2) {
-    let myPlayer = Undefined<typeof player>();
-    const m = new Matrix();
-    const c = container().withStep(() => {
-        m.tx = -c.x + 4;
-        m.ty = -c.y;
-        if (!myPlayer || myPlayer !== player) {
-            myPlayer = player;
-            player.withStep(() => game.renderer.render(player, texture, true, m));
-        }
-    });
+    RenderPlayerWithOffset.value;
+
     function rect() {
         return new Graphics().beginFill(color).drawRect(0, 0, width, height);
     }
@@ -22,10 +18,33 @@ export function mirror(width, height, color = 0xC0D6E5, lineColor = 0xD5E6F2) {
         .lineStyle(4, lineColor).moveTo(16, -3).lineTo(-3, 16)
         .lineStyle(2, lineColor).moveTo(32, -3).lineTo(-3, 32)
         .lineStyle(1, lineColor).moveTo(width + 3, height - 16).lineTo(width + 3 - 16, height);
-    g.mask = rect();
-    const texture = RenderTexture.create({ width, height });
-    const s = Sprite.from(texture);
+    const s = Sprite.from(playerTexture)
+        .withTicker(scene.endTicker)
+        .withStep(() => s.at(-c.worldTransform.tx, -c.worldTransform.ty));
     s.alpha = 0.5;
-    c.addChild(g.mask, g, s);
+    const c = container(g, s);
+    c.mask = rect().show(c);
+
     return c;
+}
+
+const RenderPlayerWithOffset = new SceneLocal(renderPlayerWithOffset, 'RenderPlayerWithOffset');
+
+function renderPlayerWithOffset() {
+    return container()
+        .withTicker(scene.endTicker)
+        .withStep(() => {
+            const p1 = scene.camera.vcpy();
+            const p2 = player.vcpy();
+            player.vround();
+            scene.camera.vround();
+
+            matrix.tx = -scene.camera.x + 4;
+            matrix.ty = -scene.camera.y;
+
+            game.renderer.render(player, playerTexture, true, matrix);
+            scene.camera.at(p1);
+            player.at(p2);
+        })
+        .show(scene.playerStage);
 }
--- src/gameObjects/oversizedDoor.ts
+++ src/gameObjects/oversizedDoor.ts
@@ -1,24 +1,36 @@
 import {subimageTextures} from "../utils/pixi/simpleSpritesheet";
 import {FinalLargeDoorBack, FinalLargeMetalDoor} from "../textures";
-import {Graphics, Sprite} from "pixi.js";
+import {DisplayObject, Graphics, Sprite} from "pixi.js";
 import {container} from "../utils/pixi/container";
 import {lerp} from "../cutscene/lerp";
 import {sleep} from "../cutscene/sleep";
 import {merge} from "../utils/object/merge";
 import {wait} from "../cutscene/wait";
 import {range} from "../utils/range";
+import {mirror} from "./mirror";
+import {ToRad} from "../utils/math/angles";
+import {move} from "../cutscene/move";
+
+function angle(i: number) {
+    return (i * 90) % 360;
+}
+
+type Door = DisplayObject & { active: boolean; complete: boolean; }
 
 export function oversizedDoor() {
     const mask = new Graphics().beginFill(0).drawRect(0, 0, 64, 64);
     const c = container(mask);
 
-    const doors = range(4).map(i => largeMetalDoor((i * 90) % 360));
+    const doors: Door[] = [
+        ...range(4).map(i => largeMetalDoor(angle(i))),
+        ...range(4).map(i => mirrorDoor(angle(i))),
+    ];
     c.addChild(...[...doors].reverse());
 
     c.withAsync(async () => {
         for (const door of doors) {
             door.active = true;
-            await wait(() => door.destroyed);
+            await wait(() => door.complete);
         }
     });
 
@@ -34,8 +46,8 @@ export function oversizedDoor() {
 
 const largeMetalDoorTxs = subimageTextures(FinalLargeMetalDoor, 3);
 
-function largeMetalDoor(angle) {
-    const s0 = merge(Sprite.from(largeMetalDoorTxs[0]), { active: false });
+function largeMetalDoor(angle: number) {
+    const s0 = merge(Sprite.from(largeMetalDoorTxs[0]), { active: false, complete: false });
     if (angle === 90 || angle === 180)
         s0.x = 64;
     if (angle === 180 || angle === 270)
@@ -52,6 +64,29 @@ function largeMetalDoor(angle) {
             await lerp(s2.pivot, 'y').to(3).over(300);
             await sleep(200);
             await lerp(s0.pivot, 'y').to(48).over(1000);
+            s0.complete = true;
             s0.destroy();
         });
+}
+
+function mirrorDoor(angle = 0) {
+    const m = merge(container(mirror(64, 64, 0xC0C2E5, 0xD9DCF2)), { active: false, complete: false });
+    new Graphics()
+        .beginFill(0x405080, 0.0825)
+        .drawRect(-3 , -3, 70, 70)
+        .drawRect(-2 , -2, 68, 68)
+        .drawRect(-1 , -1, 66, 66)
+        .show(m, 0);
+
+    return m.withAsync(async () => {
+        await wait(() => m.active);
+
+        const dx = Math.sin(angle * ToRad) * 64;
+        const dy = Math.cos(angle * ToRad) * 64;
+
+        await move(m).off(dx, dy).over(1000);
+
+        m.complete = true;
+        m.destroy();
+    });
 }
