a9f1b7d808a65dd59e43a6b5b2074708da96b0df
Hubol <hubol.gordon@gmail.com>
2023-01-08 20:17:47 -0600

    WIP light rays in final temple outer chamber

--- src/gameObjects/lightRayCrude.ts
+++ src/gameObjects/lightRayCrude.ts
@@ -6,28 +6,34 @@ import {scene} from "../igua/scene";
 import {container} from "../utils/pixi/container";
 import {CapitalLightRay} from "../textures";
 import {alphaObscureFilter} from "../utils/pixi/alphaMaskFilter";
+import {vnew} from "../utils/math/vector";
 
 const matrix = new Matrix();
 const playerTexture = RenderTexture.create({ width: 256, height: 256 });
 const shadowTexture = RenderTexture.create({ width: 256, height: 256 });
 
 export function lightRayCrude() {
-    RenderPlayerAndShadow.value;
-
     const s = Sprite.from(CapitalLightRay);
-    const shadow = Sprite.from(shadowTexture);
 
-    const c = container(s).filter(alphaObscureFilter(shadow));
+    const c = container(s).filter(makeShadowCastFilter());
     c.alpha = 0.3;
     return c;
 }
+
+export function makeShadowCastFilter() {
+    RenderPlayerAndShadow.value;
+    const shadow = Sprite.from(shadowTexture);
+    return alphaObscureFilter(shadow);
+}
+
+export const ShadowCastDirection = new SceneLocal(() => vnew().at(1, 1), 'ShadowCastDirection');
 
 const RenderPlayerAndShadow = new SceneLocal(renderPlayerAndShadow, 'RenderPlayerAndShadow');
 
 function renderPlayerAndShadow() {
     const shadow = container();
     for (let i = 1; i < 256; i++)
-        shadow.addChild(Sprite.from(playerTexture).at(i, i));
+        shadow.addChild(Sprite.from(playerTexture).at(ShadowCastDirection.value.x * i, ShadowCastDirection.value.y * i));
 
     return container()
         .withTicker(scene.endTicker)
--- src/levels/finalTempleOuter.ts
+++ src/levels/finalTempleOuter.ts
@@ -2,7 +2,7 @@ import {scene} from "../igua/scene";
 import {FinalTempleOuterArgs} from "../levelArgs";
 import {applyOgmoLevel} from "../igua/level/applyOgmoLevel";
 import {ProgressBigKey} from "../igua/data/getCompletion";
-import {DisplayObject, Texture} from "pixi.js";
+import {DisplayObject, Graphics, Texture} from "pixi.js";
 import {bigKeyMeter} from "../gameObjects/bigKey";
 import {getWorldCenter} from "../igua/gameplay/getCenter";
 import {progress} from "../igua/data/progress";
@@ -10,6 +10,8 @@ import {desertBigKeyTextures} from "./desertTemple";
 import {jungleBigKeyTextures} from "./jungleTemple";
 import {volcanoBigKeyTextures} from "./volcanoTemple";
 import {capitalBigKeyTextures} from "./capitalTemple";
+import {container} from "../utils/pixi/container";
+import {makeShadowCastFilter, ShadowCastDirection} from "../gameObjects/lightRayCrude";
 
 export function FinalTempleOuter() {
     scene.backgroundColor = 0x60B0E0;
@@ -22,6 +24,25 @@ export function FinalTempleOuter() {
     showBigKeyMeter(jungle.bigKey, jungleBigKeyTextures, level.BigKey2);
     showBigKeyMeter(volcano.bigKey, volcanoBigKeyTextures, level.BigKey3);
     showBigKeyMeter(capital.bigKey, capitalBigKeyTextures, level.BigKey4);
+
+    showLightRays();
+}
+
+function showLightRays() {
+    const g = new Graphics()
+        .lineStyle(40, 0xffffff);
+
+    for (const x of [ 48, 112, 240 ]) {
+        g.moveTo(x, 0)
+        .lineTo(x, 256)
+    }
+
+    g.alpha = 0.3;
+
+    ShadowCastDirection.value.x = 0;
+
+    const c = container(g).filter(makeShadowCastFilter()).ahead();
+    c.mask = new Graphics().beginFill(0xffffff).drawRect(16, 64, 304, 130).show();
 }
 
 function hasAllPieces(bigKey: ProgressBigKey) {
