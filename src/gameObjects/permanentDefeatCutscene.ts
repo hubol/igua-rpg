190f203b8e454fb9914a9342fa159ad64303b1f4
Hubol <hubol.gordon@gmail.com>
2023-01-27 13:30:26 -0600

    Hack to not resume boss music after permanent defeat cutscene

--- src/gameObjects/permanentDefeatCutscene.ts
+++ src/gameObjects/permanentDefeatCutscene.ts
@@ -7,6 +7,7 @@ import {WeakToSpellsInstances} from "../pixins/weakToSpells";
 import {showAll} from "../cutscene/dialog";
 import {FinalDoorFinish} from "../sounds";
 import {Screenshake} from "./earthquake";
+import {Hemaboss1} from "../musics";
 
 export async function permanentDefeatCutscene() {
     disablePlayerCollision();
@@ -25,7 +26,8 @@ export async function permanentDefeatCutscene() {
     await lerp(f, 'alpha').to(0).over(1000);
 
     await sleep(500);
-    jukebox.fadeIn(0, 1000);
+    if (jukebox.currentSong !== Hemaboss1)
+        jukebox.fadeIn(0, 1000);
     await showAll('Something great has happened.',
         'Return to the wizard of emotion for your reward.');
 
