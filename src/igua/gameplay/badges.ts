995447489a0d65b3025088c216f2e45a1eaa6ba1
Hubol <hubol.gordon@gmail.com>
2023-02-05 14:11:05 -0600

    Add names and descriptions to badges

--- src/igua/gameplay/badges.ts
+++ src/igua/gameplay/badges.ts
@@ -22,25 +22,32 @@ const badgeTemplate = {
     lungAttackPowerScale: 1,
     lungCastDistanceScale: 1,
     lungCastTimeScale: 1,
+
+    name: '?',
+    description: '?',
 }
 
 export type Badge = typeof badgeTemplate;
 
-function mk(args: Partial<Badge>): Badge {
-    return defaults(badgeTemplate, args);
+function mk(args: Partial<Badge>, name = '?', description = '?'): Badge {
+    return defaults(badgeTemplate, { ...args, name, description });
 }
 
 export const Badges = {
-    None: mk({}),
-    Shield: mk({ damageTakenScale: 1.2, duckDamageTakenScale: 0.5, duckedDamageCountsTowardsHitCount: false, duckTemporaryClawAttackPowerScale: 1.33, duckTemporaryClawAttackPowerFrameCount: 10 * 60, duckInvulnerableFrameCount: 100 }),
-    Redirect: mk({ poisonAffectsClawAttackPower: true, poisonAffectsMovementSpeed: false }),
-    Dexterous: mk({ comboClawAttackPowerScale: [0.7, 0.9, 1.5, 1.2, 1.8, 1.4, 1.4, 1.4, 1.4, 2.3, 1.4] }),
-    Cigarette: mk({ lungCastDistanceScale: 36 / 90, lungCastTimeScale: 0.925, lungAttackPowerScale: 1.67 }),
+    None: mk({}, 'Nothing'),
+    Shield: mk({ damageTakenScale: 1.2, duckDamageTakenScale: 0.5, duckedDamageCountsTowardsHitCount: false, duckTemporaryClawAttackPowerScale: 1.33, duckTemporaryClawAttackPowerFrameCount: 10 * 60, duckInvulnerableFrameCount: 100 },
+        'Shield', 'Ducking is more effective and grants a temporary Claw attack power boost, but unblocked damage is increased.'),
+    Redirect: mk({ poisonAffectsClawAttackPower: true, poisonAffectsMovementSpeed: false },
+        'Redirect Tool', 'Poison increases Claw attack power instead of speed.'),
+    Dexterous: mk({ comboClawAttackPowerScale: [0.7, 0.9, 1.5, 1.2, 1.8, 1.4, 1.4, 1.4, 1.4, 2.3, 1.4] },
+        'Dexterous Twig', 'Claw attack power is reduced, but temporarily increases with consecutive attacks.'),
+    Cigarette: mk({ lungCastDistanceScale: 36 / 90, lungCastTimeScale: 0.925, lungAttackPowerScale: 1.67 },
+        'Unusual Cigarette', 'Lung cast distance is reduced, but Lung attack power is increased.'),
 
     // TODO These are pretty boring...
     Heavy: mk({ movementSpeedScale: 0.625, baseAttackPowerScale: 1.5 }),
     Oxygen: mk({ comboClawAttackPowerScale: [0.5], lungAttackPowerScale: 1.825 }),
-    Cigar: mk({ lungCastTimeScale: 1.33, lungAttackPowerScale: 1.67 }),
+    Cigar: mk({ lungCastTimeScale: 2, lungAttackPowerScale: 2.67 }),
 }
 
 export type BadgeId = keyof typeof Badges;
